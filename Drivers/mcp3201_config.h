e86564e6e531ad05cb97bcf60afc879a7fdbae7d
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-02-08 20:25:22 +0330

    chore: initial project setup

---
+++ Drivers/mcp3201_config.h
@@ -0,0 +1,12 @@
+#ifndef MCP3201_CONFIG_INCLUDED
+#define MCP3201_CONFIG_INCLUDED
+
+#ifndef MCP3201_CONFIG
+#define MCP3201_CONFIG    
+    #define MCP3201_VREF        5000U     //mv
+     
+    #warning "MCP3201_CONFIG is not defined; default configuration will be used."
+#endif
+
+#endif
+
