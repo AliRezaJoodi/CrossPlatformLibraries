ec3e0e5780b9829335a94715b69ab450b4237d3f
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-02-14 06:32:38 +0330

    Create button_config.h

---
+++ Drivers/button_config.h
@@ -0,0 +1,41 @@
+// GitHub Account: GitHub.com/AliRezaJoodi
+
+#ifndef BUTTON_CONFIG_INCLUDED
+#define BUTTON_CONFIG_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @note Default hardware macros can be overridden by the user.
+ */ 
+#include "hardware.h"
+
+typedef enum {
+    BUTTON_ACTIVE_LOW   = 0U,
+    BUTTON_ACTIVE_HIGH  = 1U
+} ButtonActiveLevel_t;
+
+typedef enum {
+    BUTTON_MODE_FLOATING   = 0U << 1,
+    BUTTON_MODE_PULLUP     = 1U << 1,
+    BUTTON_MODE_PULLDOWN   = 2U << 1
+} ButtonInputMode_t;
+
+#ifndef BUTTON_CONFIG
+#define BUTTON_CONFIG
+//    #define BUTTON_ACTIVE               0U 
+//    #define BUTTON_INACTIVE             BUTTON_MODE_PULLUP
+    #define BUTTON_SINGLE_CLICK_LAG     30U          // Single Click Lag, Value: 0~65535, Unit: ms  
+    #define BUTTON_AUTO_REPEAT_LAG      500U         // Auto Repeat Lag, Value: 0~65535
+    #define BUTTON_LONG_PRESS_LAG       10000U       // Long Press Lag, Value: 0~65535
+     
+    #warning "BUTTON_HARDWARE is not defined; default configuration will be used."
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
