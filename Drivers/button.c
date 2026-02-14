6ac82c80e2b10f5ec9a25874decdab5f4e8c428e
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-02-14 06:33:10 +0330

    Update button lib

--- Drivers/button.c
+++ Drivers/button.c
@@ -2,34 +2,41 @@
 
 #include <button.h>
 
+#define Button_GetActiveStatus(btn)     ((btn)->config & 0x01)
+
+////*************************************************
+//static inline uint8_t Button_GetActiveStatus(Button_t *btn){
+//    return ((btn->config) & 0x01);
+//}
+
 //*************************************************
 void Button_Config(Button_t *btn){
-    CLEAR_BIT(*btn->hw_ddr, btn->hw_bit);
-//    WRITE_BIT(*btn->hw_port, btn->hw_bit, !BUTTON_PRESSED);
-    btn->status  = 0;
+    Button_ConfigPin(btn);
+
+    btn->state = 0;
     btn->counter = 0;
 }
 
-//*************************************************
-void Button_SetPullUp(Button_t *btn, uint8_t enable){
-    WRITE_BIT(*btn->hw_port, btn->hw_bit, enable);
-}
+////*************************************************
+//void Button_SetPullUp(Button_t *btn, uint8_t enable){
+//    WRITE_BIT(*btn->hw_port, btn->hw_bit, enable);
+//}
 
 //*************************************************
 uint8_t Button_GetSingleClick(Button_t *btn){
-    if ( GET_BIT(*btn->hw_pin, btn->hw_bit) != BUTTON_PRESSED ){
-        if(btn->status == 1){
+    if ( Button_GetPin(btn) != Button_GetActiveStatus(btn) ){
+        if(btn->state == 1){
             delay_ms(BUTTON_SINGLE_CLICK_LAG);
-            if (GET_BIT(*btn->hw_pin, btn->hw_bit) != BUTTON_PRESSED){
-                btn->status = 0;
+            if ( Button_GetPin(btn) != Button_GetActiveStatus(btn) ){
+                btn->state = 0;
             } 
         }
     }
     else{
-        if(btn->status == 0){
+        if(btn->state == 0){
             delay_ms(BUTTON_SINGLE_CLICK_LAG);
-            if (GET_BIT(*btn->hw_pin, btn->hw_bit) == BUTTON_PRESSED){
-                btn->status = 1;
+            if ( Button_GetPin(btn) == Button_GetActiveStatus(btn) ){
+                btn->state = 1;
                 return 1;
             }
         }
@@ -40,7 +47,7 @@ uint8_t Button_GetSingleClick(Button_t *btn){
 
 //*************************************************
 uint8_t Button_GetAutoRepeat_NonBlocking(Button_t *btn){
-    if (GET_BIT(*btn->hw_pin, btn->hw_bit) == BUTTON_PRESSED){
+    if (Button_GetPin(btn) == Button_GetActiveStatus(btn)){
         btn->counter++;
 
         if (btn->counter >= BUTTON_AUTO_REPEAT_LAG){
@@ -57,18 +64,18 @@ uint8_t Button_GetAutoRepeat_NonBlocking(Button_t *btn){
 
 //*************************************************
 uint8_t Button_GetLongPress_NonBlocking(Button_t *btn){
-    if (GET_BIT(*btn->hw_pin, btn->hw_bit) == BUTTON_PRESSED){
+    if (Button_GetPin(btn) == Button_GetActiveStatus(btn)){
         if (btn->counter < BUTTON_LONG_PRESS_LAG)
             btn->counter++;
 
-        if ((btn->counter >= BUTTON_LONG_PRESS_LAG) && (btn->status == 0)){
-            btn->status = 1;
+        if ((btn->counter >= BUTTON_LONG_PRESS_LAG) && (btn->state == 0)){
+            btn->state = 1;
             return 1;
         }
     }
     else{
         btn->counter = 0;
-        btn->status = 0;
+        btn->state = 0;
     }
 
     return 0;
