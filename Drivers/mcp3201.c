00dd8b6cabec8df4aed2ab6d3f33060f1aaa04e4
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-02-15 14:24:38 +0330

    chore: update mcp3201 lib

--- Drivers/mcp3201.c
+++ Drivers/mcp3201.c
@@ -8,8 +8,8 @@ static const float  MCP3201_GAIN = (float)(MCP3201_VREF) / (float)(MCP3201_RESOL
 
 //********************************************************
 void MCP3201_Init(MCP3201_t *mcp){
-    MCP3201_CS_Init(mcp); 
-    MCP3201_CS_WritePin(mcp, 1);    // Idle bus   
+    MCP3201_CS_InitPin(mcp);
+    MCP3201_CS_WritePin(mcp, 1);    // Idle bus
 }
 
 //********************************************************
@@ -17,17 +17,17 @@ uint16_t MCP3201_GetCounts(MCP3201_t *mcp){
     uint16_t value = 0;
     uint8_t msb = 0;
     uint8_t lsb = 0;
-    
+
     MCP3201_CS_WritePin(mcp, 0);
-    msb = MCP3201_SPI_Transfer(0xFF); 
+    msb = MCP3201_SPI_Transfer(0xFF);
     lsb = MCP3201_SPI_Transfer(0xFF);
     MCP3201_CS_WritePin(mcp, 1);
-    
+
     msb = msb & 0x1FU;
-    lsb = lsb & 0xFEU; 
+    lsb = lsb & 0xFEU;
     value = ( (uint16_t)msb << 8U ) | lsb;
     value = value >> 1U;
-       
+
     return value;
 }
 
@@ -35,7 +35,7 @@ uint16_t MCP3201_GetCounts(MCP3201_t *mcp){
 float MCP3201_GetMilliVolt(MCP3201_t *mcp){
     uint16_t counts = 0;
 
-    counts = MCP3201_GetCounts(mcp);   
+    counts = MCP3201_GetCounts(mcp);
     return (float)(counts) * MCP3201_GAIN;
 }
 
@@ -44,7 +44,7 @@ float MCP3201_ConvertCountsToMilliVolt(uint16_t counts){
     if (counts >= MCP3201_RESOLUTION){
         counts = MCP3201_RESOLUTION - 1;
     }
-    
+
     return (float)(counts) * MCP3201_GAIN;
 }
 
--- Drivers/mcp3201_config.h
+++ Drivers/mcp3201_config.h
@@ -1,12 +1,14 @@
 #ifndef MCP3201_CONFIG_INCLUDED
 #define MCP3201_CONFIG_INCLUDED
 
-#include "hardware.h"  // Default hardware macros can be overridden by the user
+/** @brief Include user-overridable macros. */
+#include "hardware.h"
 
+/** @brief Default configuration (can be overridden). */
 #ifndef MCP3201_CONFIG
-#define MCP3201_CONFIG    
-    #define MCP3201_VREF        5000U     //mv
-     
+#define MCP3201_CONFIG
+    #define MCP3201_VREF        5000U     /**< Ref Voltage (mv) */
+
     #warning "MCP3201_CONFIG is not defined; default configuration will be used."
 #endif
 
