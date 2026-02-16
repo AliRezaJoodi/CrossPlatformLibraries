61bdc6c6032d61c302dbcb8e4bfc74b204068951
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-02-16 21:37:25 +0330

    chore: initial lib setup, seg7_4digit_encoder

---
+++ Modules/seg7_4digit_encoder.h
@@ -0,0 +1,14 @@
+// GitHub Account: GitHub.com/AliRezaJoodi
+
+#ifndef SEG7_4DIGIT_ENCODER_INCLOUDED
+#define SEG7_4DIGIT_ENCODER_INCLOUDED
+
+#include <stdint.h>
+
+#include "seg7_1digit_encoder.h"
+
+uint8_t Seg7_4Digit_EncodeFloat_2DP(float value, uint8_t *seg);
+uint8_t Seg7_4Digit_EncodeFloat_1DP(float value, uint8_t *seg);
+void    Seg7_4Digit_ToggleSegments(uint8_t *seg);
+
+#endif
