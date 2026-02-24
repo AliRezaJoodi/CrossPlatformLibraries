7c261b7746c52be07c8e14d24d3ad831bf0c833d
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-02-24 06:41:54 +0330

    chore: rename lib, compiler_port

--- Platforms/AVR/compiler_port.h
+++ Platforms/AVR/compiler_port.h
@@ -1,9 +1,13 @@
 
-#ifndef COMPILER_DETECTOR_INCLUDED
-#define COMPILER_DETECTOR_INCLUDED
+#ifndef COMPILER_PORT_INCLUDED
+#define COMPILER_PORT_INCLUDED
 
 #if defined(__GNUC__)
     #define COMPILER_GCC 1
+
+    #ifndef F_CPU
+        #error "F_CPU must be defined before including this library"
+    #endif
 
     #include <avr/io.h>
     #include <util/delay.h> // F_CPU must be defined before including this header for accurate delay calculations
@@ -19,10 +23,18 @@
 
     #define DELAY_MS(ms) delay_ms(ms)   /* Macro for delay in milliseconds */
     #define DELAY_US(us) delay_us(us)   /* Macro for delay in microseconds */
+
 #elif defined(__ICCAVR__)
     #define COMPILER_IAR 1
 
+    #ifndef F_CPU
+    #error "F_CPU must be defined before including this library"
+    #endif
+
     #include <ioavr.h>
+
+    #define DELAY_MS(ms) __delay_cycles((F_CPU/1000UL)*(ms))
+    #define DELAY_US(us) __delay_cycles((F_CPU/1000000UL)*(us))
 
 #else
     #error "Unsupported Compiler"
