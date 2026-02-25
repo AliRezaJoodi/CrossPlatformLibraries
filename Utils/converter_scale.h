99c287c3ac1b26196a9be5d079c741871c30bd32
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-02-26 02:49:41 +0330

    Update converter_scale.h

--- Utils/converter_scale.h
+++ Utils/converter_scale.h
@@ -1,4 +1,17 @@
-// GitHub Account: GitHub.com/AliRezaJoodi
+/*
+ * @brief Linear scale conversion utilities for ADC values.
+ *
+ * @warning
+ * These functions use floating-point operations (float).
+ * On MCUs without a hardware FPU (e.g., most 8-bit AVR devices),
+ * floating-point arithmetic is implemented in software and may:
+ *  - Consume significantly more CPU cycles
+ *  - Increase flash memory usage
+ *  - Increase temporary stack usage
+ *
+ * @author  AliReza Joodi
+ * @see     https://github.com/AliRezaJoodi
+ */
 
 #ifndef CONVERTER_SCALE_INCLUDED
 #define CONVERTER_SCALE_INCLUDED
@@ -45,16 +58,32 @@ static inline uint8_t Convert_0to3V3_0to255(float voltage) {
     return (uint8_t)(voltage * 77.2727f);
 }
 
+static inline float Convert_0to4095_0to5V(uint16_t counts){
+    return (float)counts * 0.0012210012f;
+}
+
+static inline float Convert_0to4095_0to5000mV(uint16_t counts){
+    return (float)counts * 1.2210012f;
+}
+
+static inline float Convert_0to4095_0to3V3(uint16_t counts){
+    return (float)counts * 0.0008058608f;
+}
+
+static inline float Convert_0to4095_0to3300mV(uint16_t counts){
+    return (float)counts * 0.8058608f;
+}
+
 static inline float Convert_0to1023_0to5V(uint16_t counts) {
     return (float)counts * 0.0048875855f;
 }
 
-static inline float Convert_0to255_0to5V(uint8_t counts) {
-    return (float)counts * 0.019607843f;
-}
-
 static inline float Convert_0to1023_0to2V56(uint16_t counts) {
     return (float)counts * 0.002502444f;
+}
+
+static inline float Convert_0to255_0to5V(uint8_t counts) {
+    return (float)counts * 0.019607843f;
 }
 
 static inline float Convert_0to255_0to2V56(uint8_t counts) {
