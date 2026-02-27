adca6dbecb59cf53330918341569816d0499172f
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-02-27 18:30:11 +0330

    chore: initial lib setup, tm1638

---
+++ Platforms/AVR/tm1638_port.h
@@ -0,0 +1,19 @@
+#ifndef TM1638_PORT_INCLUDED
+#define TM1638_PORT_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include "compiler_port.h"
+#include "utility_bit.h"
+
+#define TM1638_DELAY_US(VALUE)          DELAY_US(VALUE)
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
