c3d76878419cfbdb7299c5eff9f8be8a240be66a
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-03-03 02:08:07 +0330

    Create tm1637_hw.h

---
+++ Platforms/AVR/tm1637_hw.h
@@ -0,0 +1,21 @@
+#ifndef TM1637_HW_INCLUDED
+#define TM1637_HW_INCLUDED
+
+#include "compiler_port.h"
+
+#ifndef TM1637_HARDWARE_EXTRA
+#define TM1637_HARDWARE_EXTRA
+    #define TM1637_TM1_CLK_DDR         DDRC
+    #define TM1637_TM1_CLK_PORT        PORTC
+    #define TM1637_TM1_CLK_PIN         PINC
+    #define TM1637_TM1_CLK_BIT         0
+
+    #define TM1637_TM1_DIO_DDR         DDRC
+    #define TM1637_TM1_DIO_PORT        PORTC
+    #define TM1637_TM1_DIO_PIN         PINC
+    #define TM1637_TM1_DIO_BIT         1
+
+    //#warning "TM1637_HARDWARE_EXTRA is not defined; default configuration will be used."
+#endif
+
+#endif
