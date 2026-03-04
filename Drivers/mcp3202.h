d0bdc0937f8602495bb2dccc0f2dd1be00694c97
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-03-04 17:30:51 +0330

    chore: update mcp3202

--- Drivers/mcp3202.h
+++ Drivers/mcp3202.h
@@ -1,8 +1,14 @@
-// GitHub Account: GitHub.com/AliRezaJoodi
-// This library use of the SPI interfacing
-// SPI Clock Phase: Cycle Start
-// SPI Clock Polarity: Low
-// SPI Data Order: MSB First
+/**
+ * @brief   MCP3202 12-bit ADC driver
+ *
+ * This library provides basic functions to interface with the MCP3202 ADC via SPI.
+ *
+ * @note
+ * SPI must be configured by the user before using this library.
+ *
+ * @author  AliReza Joodi
+ * @see     https://github.com/AliRezaJoodi
+ */
 
 #ifndef MCP3202_INCLUDED
 #define MCP3202_INCLUDED
@@ -12,7 +18,6 @@ extern "C" {
 #endif
 
 #include <stdint.h>
-#include <spi.h>
 #include "utility_bit.h"
 #include "mcp3202_port.h"
 
@@ -21,7 +26,25 @@ extern "C" {
 #define MCP3202_CH0CH1      2U  /**< CH0(IN+), CH1(IN-)*/
 #define MCP3202_CH1CH0      3U  /**< CH1(IN+), CH0(IN-)*/
 
+/**
+ * @brief   Initialize the MCP3202 instance
+ *
+ * This function initializes the MCP3202 by configuring the CS pin
+ * and setting it to idle state.
+ *
+ * @param   mcp   Pointer to a valid MCP3202_t instance
+ */
 void MCP3202_Init(MCP3202_t *mcp);
+
+/**
+ * @brief   Read raw ADC counts from MCP3202
+ *
+ * This function performs an SPI transaction to read the 12-bit ADC value
+ * from the MCP3202 device.
+ *
+ * @param   mcp   Pointer to a valid MCP3202_t instance
+ * @return  12-bit ADC value as uint16_t (0 to 4095)
+ */
 uint16_t MCP3202_GetCounts(MCP3202_t *mcp, uint8_t ch);
 
 #ifdef __cplusplus
--- Platforms/AVR/mcp3202_hw.h
+++ Platforms/AVR/mcp3202_hw.h
@@ -1,8 +1,6 @@
 #ifndef MCP3202_HW_INCLUDED
 #define MCP3202_HW_INCLUDED
 
-#include "compiler_port.h"
-
 #ifndef MCP3202_HARDWARE_EXTRA
 #define MCP3202_HARDWARE_EXTRA
     #define MCP3202_CS_DDR          DDRB
