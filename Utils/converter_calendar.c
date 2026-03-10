5e41b62bee029f86e87a3baebc690a066c203cf0
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-03-10 23:15:05 +0330

    chore: update converter_calendar

--- Utils/converter_calendar.c
+++ Utils/converter_calendar.c
@@ -1,3 +1,73 @@
-//#include "converter_calendar.h"
+#include "converter_calendar.h"
+
+flash uint8_t table_jalaali[6][12]={
+    {11,10,10, 9, 9, 9, 8, 9, 9,10,11, 9},
+    {20,20,21,21,22,22,22,22,21,21,20,19},
+    {11,10,10, 9, 9, 9, 8, 9, 9,10,11,10},
+    {20,20,21,21,22,22,22,22,21,21,20,19},
+    {12,11,11,10,10,10, 9,10,10,11,12,10},
+    {19,19,20,20,21,21,21,21,20,20,19,18}
+};
+
+flash uint8_t table_gregorian[6][12]={
+    {20,19,19,19,20,20,21,21,21,21,20,20},
+    {10,11,10,12,11,11,10,10,10, 9,10,10},
+    {19,18,20,20,21,21,22,22,22,22,21,21},
+    {11,12,10,11,10,10, 9, 9, 9, 8, 9, 9},
+    {20,19,20,20,21,21,22,22,22,22,21,21},
+    {10,11, 9,11,10,10, 9, 9, 9, 8, 9, 9}
+};
+
+void ConvertCalendar_GregorianToJalaali(date_t *gc, date_t *jc){
+    uint8_t k,t1,t2;
+
+    k = gc->year % 4;
+
+    if(k == 3){k = 2;};
+    k *= 2;
+    t1 = table_gregorian[k][gc->month - 1];
+    t2 = table_gregorian[k+1][gc->month - 1];
+    if(gc->month < 3 || (gc->month == 3 && gc->day <= table_gregorian[k][gc->month-1])){jc->year = gc->year + 78;}
+        else{jc->year = gc->year + 79;}
+    jc->year = jc->year % 100;
+
+    if(gc->day <= t1){
+       jc->day = gc->day + t2;
+       jc->month = (gc->month + 8) % 12 + 1;
+    }
+    else{
+       jc->day = gc->day - t1;
+       jc->month = (gc->month + 9) % 12 + 1;
+    }
+}
+
+void ConvertCalendar_JalaaliToGregorian(date_t *jc, date_t *gc){
+    uint8_t k, t1, t2;
+
+    jc->year += 100;
+    k = jc->year % 4;
+
+    if(k == 0){k = 2;}
+    else{k = k + k;}
+
+    t1 = table_jalaali[k-2][jc->month - 1];
+    t2 = table_jalaali[k-1][jc->month - 1];
+
+    if(jc->month < 10 || (jc->month == 10 && jc->day <= table_jalaali[k-2][jc->month-1])){gc->year = jc->year - 79;}
+        else{gc->year = jc->year - 78;}
+
+    gc->year = gc->year % 100;
+
+    if(jc->day <= t1){
+       gc->day = jc->day + t2;
+       gc->month = (jc -> month + 1) % 12 + 1;
+    }
+    else{
+       gc->day = jc->day - t1;
+       gc->month = (jc->month + 2) % 12 + 1;
+    }
+
+    jc->year -= 100;
+}
 
 
