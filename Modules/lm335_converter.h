4744b1a619965c6629b1cd0c2711e5c9fe28df11
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-03-11 05:01:27 +0330

    Update lm335_converter.h

--- Modules/lm335_converter.h
+++ Modules/lm335_converter.h
@@ -12,19 +12,19 @@
  * @see     https://github.com/AliRezaJoodi
  */
 
-#ifndef LM335_INCLUDED
-#define LM335_INCLUDED
+#ifndef LM335_CONVERTER_INCLUDED
+#define LM335_CONVERTER_INCLUDED
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-static inline float LM335_ConvertMilliVoltToTemp(float volt_mv){
-    return (volt_mv * 0.1f);
+static inline float LM335_ConvertMilliVoltToTemp(float mv){
+    return (mv * 0.1f);
 }
 
-static inline float LM335_ConvertVoltToTemp(float volt_v){
-    return (volt_v * 1000 * 0.1f);
+static inline float LM335_ConvertVoltToTemp(float voltage){
+    return (voltage * 1000 * 0.1f);
 }
 
 #ifdef __cplusplus
