71d2b32dc75109e5dcbb84c96f8bc522bec4f326
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-03-13 01:58:45 +0330

    Update adc_converter.h

--- Modules/adc_converter.h
+++ Modules/adc_converter.h
@@ -55,12 +55,7 @@ static inline float ADC_Convert_12Bit3V3_mV(uint16_t counts){
 //}
 
 static inline uint16_t ADC_Convert_10Bit5V_mV(uint16_t counts){
-//    return (uint16_t)(((uint32_t)counts * 1250U) >> 8);
-    return (counts << 2)
-         + (counts >> 1)
-         + (counts >> 2)
-         + (counts >> 3)
-         + (counts >> 7);
+    return (uint16_t)(((uint32_t)counts * 1250U) >> 8);
 }
 
 //static inline float ADC_Convert_10Bit5V_mV_f32(uint16_t counts){
@@ -71,20 +66,30 @@ static inline uint16_t ADC_Convert_10Bit4V096_mV(uint16_t counts){
     return counts << 2;
 }
 
-static inline uint32_t ADC_Convert_10Bit2V56_uV(uint16_t counts){
-    return ((uint32_t)counts * 2500UL);
+//static inline uint32_t ADC_Convert_10Bit2V56_uV(uint16_t counts){
+//    return ((uint32_t)counts * 2500UL);
+//}
+
+static inline uint16_t ADC_Convert_10Bit2V56_mV(uint16_t counts){
+    return (uint16_t)((counts * 5U) >> 1);
 }
 
-static inline float ADC_Convert_10Bit2V56_mV(uint16_t counts){
-    return (float)counts * 2.5f;
-}
+//static inline float ADC_Convert_10Bit2V56_mV_(uint16_t counts){
+//    return (float)counts * 2.5f;
+//}
 
 static inline uint16_t ADC_Convert_10Bit2V048_mV(uint16_t counts){
     return counts << 1;
 }
 
+static inline uint16_t ADC_Convert_8Bit5V_mV(uint8_t counts) {
+    return ((uint32_t)counts * 5000U) >> 8;
+    //return ((uint32_t)counts * 625U) >> 5;
+
+}
+
 static inline float Convert_0to255_0to5V(uint8_t counts) {
-    return (float)counts * 0.019607843f;
+    return (float)counts * 19.53125f;
 }
 
 static inline float Convert_0to255_0to2V56(uint8_t counts) {
