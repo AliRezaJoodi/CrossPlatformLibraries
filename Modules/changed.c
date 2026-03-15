5da95445957dda051a986d0c15d1a705d2cbcd94
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-03-15 11:02:43 +0330

    chore: update changed

---
+++ Modules/changed.c
@@ -0,0 +1,32 @@
+#include "changed.h"
+
+/********************************************************************************/
+uint8_t Changed_Exact(uint16_t value, uint16_t *previous){
+    if (value == *previous){
+        return 0;
+    }
+
+    *previous = value;
+    return 1;
+}
+
+/********************************************************************************/
+uint8_t Changed_Threshold(uint16_t value, uint16_t *previous, uint16_t threshold){
+    uint16_t prev = *previous;
+    uint16_t diff = 0;
+
+    if (value > prev){
+        diff = value - prev;
+    }
+    else {
+        diff = prev - value;
+    }
+
+    if (diff < threshold){
+        return 0;
+    }
+
+    *previous = value;
+    return 1;
+}
+
--- Modules/changed.h
+++ Modules/changed.h
@@ -32,14 +32,7 @@ extern "C" {
  *         - 1 : Value has changed (previous updated)
  *         - 0 : No change detected
  */
-static inline uint8_t Changed_Exact(uint16_t value, uint16_t *previous){
-    if (value == *previous){
-        return 0;
-    }
-
-    *previous = value;
-    return 1;
-}
+uint8_t Changed_Exact(uint16_t value, uint16_t *previous);
 
 /**
  * @brief Checks if a 16-bit value has changed by at least a given threshold.
@@ -55,24 +48,7 @@ static inline uint8_t Changed_Exact(uint16_t value, uint16_t *previous){
  *
  * @return 1 if the difference is >= threshold, 0 otherwise.
  */
-static inline uint8_t Changed_Threshold(uint16_t value, uint16_t *previous, uint16_t threshold){
-    uint16_t prev = *previous;
-    uint16_t diff = 0;
-
-    if (value > prev){
-        diff = value - prev;
-    }
-    else {
-        diff = prev - value;
-    }
-
-    if (diff < threshold){
-        return 0;
-    }
-
-    *previous = value;
-    return 1;
-}
+uint8_t Changed_Threshold(uint16_t value, uint16_t *previous, uint16_t threshold);
 
 #ifdef __cplusplus
 }
