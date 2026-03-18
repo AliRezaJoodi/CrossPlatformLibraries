8ab60d790274b96bf536f6589858df2432215b79
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-03-18 03:57:01 +0330

    chore: update controller_onoff

--- Modules/controller_onoff.c
+++ Modules/controller_onoff.c
@@ -1,52 +1,66 @@
 // GitHub Account:  GitHub.com/AliRezaJoodi
 
-#include <controller_onoff.h>
+#include "controller_onoff.h"
 
 //******************************************
-char Controller_OnOff_Heater(float sp,float pv, float hystersis){
-    float error=0;
-    static char heater=0;
-    
-    error=sp-pv;
-    if(error>(hystersis/2)){heater=HEATER_ACTIVE;}   
-        else if(error<-(hystersis/2)){heater=!HEATER_ACTIVE;};
-     
-    return heater;        
-}
+uint8_t Controller_OnOff(uint16_t pv, uint16_t sp, uint16_t hystersis){
+    //uint16_t error = 0;
 
-//******************************************
-char Controller_OnOff2_Heater(float sp,float pv, float hystersis){
-    float error=0;
-    static char heater=0;
-    
-    error=sp-pv;
-    if(error>(hystersis/2)){heater=HEATER_ACTIVE;}   
-        else if(error<0){heater=!HEATER_ACTIVE;};
-     
-    return heater;        
-}
+    if(pv < (sp - hystersis)){
+        return 1;
+    }
+    else if(pv > (sp + hystersis)){
+        return 2;
+    };
 
-//******************************************
-char Controller_OnOff_Cooler(float sp,float pv, float hystersis){
-    float error=0;
-    static char cooler=0;
-    
-    error=sp-pv;
-    if(error<-(hystersis/2)){cooler=COOLER_ACTIVE;}
-        else if(error>(hystersis/2)){cooler=!COOLER_ACTIVE;};   
-    
-    return cooler;        
+    return 0;
 }
-
-//******************************************
-char Controller_OnOff2_Cooler(float sp,float pv, float hystersis){
-    float error=0;
-    static char cooler=0;
-    
-    error=sp-pv;
-    if(error<-(hystersis/2)){cooler=COOLER_ACTIVE;}
-        else if(error>0){cooler=!COOLER_ACTIVE;};   
-    
-    return cooler;        
-}
-
+//
+////******************************************
+//char Controller_OnOff_Heater(float sp,float pv, float hystersis){
+//    float error=0;
+//    static char heater=0;
+//
+//    error=sp-pv;
+//    if(error>(hystersis/2)){heater=HEATER_ACTIVE;}
+//        else if(error<-(hystersis/2)){heater=!HEATER_ACTIVE;};
+//
+//    return heater;
+//}
+//
+////******************************************
+//char Controller_OnOff2_Heater(float sp,float pv, float hystersis){
+//    float error=0;
+//    static char heater=0;
+//
+//    error=sp-pv;
+//    if(error>(hystersis/2)){heater=HEATER_ACTIVE;}
+//        else if(error<0){heater=!HEATER_ACTIVE;};
+//
+//    return heater;
+//}
+//
+////******************************************
+//char Controller_OnOff_Cooler(float sp,float pv, float hystersis){
+//    float error=0;
+//    static char cooler=0;
+//
+//    error=sp-pv;
+//    if(error<-(hystersis/2)){cooler=COOLER_ACTIVE;}
+//        else if(error>(hystersis/2)){cooler=!COOLER_ACTIVE;};
+//
+//    return cooler;
+//}
+//
+////******************************************
+//char Controller_OnOff2_Cooler(float sp,float pv, float hystersis){
+//    float error=0;
+//    static char cooler=0;
+//
+//    error=sp-pv;
+//    if(error<-(hystersis/2)){cooler=COOLER_ACTIVE;}
+//        else if(error>0){cooler=!COOLER_ACTIVE;};
+//
+//    return cooler;
+//}
+//
--- Modules/controller_onoff.h
+++ Modules/controller_onoff.h
@@ -9,9 +9,13 @@
     #define COOLER_ACTIVE         1
 #endif
 
-char Controller_OnOff_Heater(float sp,float pv, float hystersis);
-char Controller_OnOff2_Heater(float sp,float pv, float hystersis);
-char Controller_OnOff_Cooler(float sp,float pv, float hystersis);
-char Controller_OnOff2_Cooler(float sp,float pv, float hystersis);
+#include <stdint.h>
+
+uint8_t Controller_OnOff(uint16_t pv, uint16_t sp, uint16_t hystersis);
+
+//char Controller_OnOff_Heater(float sp,float pv, float hystersis);
+//char Controller_OnOff2_Heater(float sp,float pv, float hystersis);
+//char Controller_OnOff_Cooler(float sp,float pv, float hystersis);
+//char Controller_OnOff2_Cooler(float sp,float pv, float hystersis);
 
 #endif
