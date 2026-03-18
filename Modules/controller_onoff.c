443752bd943199de23a5da6814427ced51fc17ab
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-03-18 21:50:48 +0330

    chore: update controller_onoff

--- Modules/controller_onoff.c
+++ Modules/controller_onoff.c
@@ -3,64 +3,17 @@
 #include "controller_onoff.h"
 
 //******************************************
-uint8_t Controller_OnOff(uint16_t pv, uint16_t sp, uint16_t hystersis){
-    //uint16_t error = 0;
+uint8_t Controller_OnOff(uint16_t pv, uint16_t sp, uint16_t hys){
+    if(sp < hys){
+        return 3;   // error
+    }
 
-    if(pv < (sp - hystersis)){
+    if(pv < (sp - hys)){
         return 1;
     }
-    else if(pv > (sp + hystersis)){
+    else if(pv > (sp + hys)){
         return 2;
-    };
+    }
 
     return 0;
 }
-//
-////******************************************
-//char Controller_OnOff_Heater(float sp,float pv, float hystersis){
-//    float error=0;
-//    static char heater=0;
-//
-//    error=sp-pv;
-//    if(error>(hystersis/2)){heater=HEATER_ACTIVE;}
-//        else if(error<-(hystersis/2)){heater=!HEATER_ACTIVE;};
-//
-//    return heater;
-//}
-//
-////******************************************
-//char Controller_OnOff2_Heater(float sp,float pv, float hystersis){
-//    float error=0;
-//    static char heater=0;
-//
-//    error=sp-pv;
-//    if(error>(hystersis/2)){heater=HEATER_ACTIVE;}
-//        else if(error<0){heater=!HEATER_ACTIVE;};
-//
-//    return heater;
-//}
-//
-////******************************************
-//char Controller_OnOff_Cooler(float sp,float pv, float hystersis){
-//    float error=0;
-//    static char cooler=0;
-//
-//    error=sp-pv;
-//    if(error<-(hystersis/2)){cooler=COOLER_ACTIVE;}
-//        else if(error>(hystersis/2)){cooler=!COOLER_ACTIVE;};
-//
-//    return cooler;
-//}
-//
-////******************************************
-//char Controller_OnOff2_Cooler(float sp,float pv, float hystersis){
-//    float error=0;
-//    static char cooler=0;
-//
-//    error=sp-pv;
-//    if(error<-(hystersis/2)){cooler=COOLER_ACTIVE;}
-//        else if(error>0){cooler=!COOLER_ACTIVE;};
-//
-//    return cooler;
-//}
-//
--- Modules/controller_onoff.h
+++ Modules/controller_onoff.h
@@ -3,19 +3,17 @@
 #ifndef CONTROLLER_ONOFF_INCLUDED
 #define CONTROLLER_ONOFF_INCLUDED
 
-#ifndef CONTROLLER_ONOFF_HARDWARE
-#define CONTROLLER_ONOFF_HARDWARE
-    #define HEATER_ACTIVE         1
-    #define COOLER_ACTIVE         1
+#ifdef __cplusplus
+extern "C" {
 #endif
 
 #include <stdint.h>
 
-uint8_t Controller_OnOff(uint16_t pv, uint16_t sp, uint16_t hystersis);
+uint8_t Controller_OnOff(uint16_t pv, uint16_t sp, uint16_t hys);
 
-//char Controller_OnOff_Heater(float sp,float pv, float hystersis);
-//char Controller_OnOff2_Heater(float sp,float pv, float hystersis);
-//char Controller_OnOff_Cooler(float sp,float pv, float hystersis);
-//char Controller_OnOff2_Cooler(float sp,float pv, float hystersis);
+#ifdef __cplusplus
+}
+#endif
 
 #endif
+
