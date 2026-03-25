42fdf6ec3fd517b58d3693e4c46c9070e2892adc
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-03-25 05:58:06 +0330

    chore: update controller_pid

--- Modules/controller_pid.c
+++ Modules/controller_pid.c
@@ -1,39 +1,81 @@
 // GitHub Account:  GitHub.com/AliRezaJoodi
 
+#include "hardware.h"
 #include "controller_pid.h"
 
 int32_t Ctrl_PID_Update2(CtrlPID_t *p){
     int32_t error;
-    int32_t value_p;
-    int32_t value_d;
+    int32_t p_term;
+    int32_t i_term;
+    int32_t d_term;
     int32_t output;
 
     // Calculate error
     error = p->sp - p->pv;
 
     // Proportional term
-    value_p = (p->kp * error) >> p->scale;
+    p_term = (error * p->kp) >> p->scale;
 
-    // Integral term accumulation
-    p->value_i += (p->ki * error) >> p->scale;
+    // Integral term accumulation (with dt)
+    p->i_sum += ((error << p->dt) >> 10);
+    i_term = (p->i_sum * p->ki) >>  p->scale;
 
     // Anti-windup: limit integral term
-    if (p->value_i > p->output_max) p->value_i = p->output_max;
-    if (p->value_i < p->output_min) p->value_i = p->output_min;
+//    if (value_i2 > p->output_max) {value_i2 = p->output_max;}
+//    else if (value_i2 < p->output_min) {value_i2 = p->output_min;}
 
-    // Derivative term
-    value_d = (p->kd * (error - p->error_last)) >> p->scale;
+    // Derivative term (with dt)
+    d_term = ((((error - p->error_last) << 10) >> p->dt) * p->kd) >> p->scale;
 
     // Store current error for next iteration
     p->error_last = error;
 
     // Compute PID output
-    output = value_p + p->value_i + value_d;
-    if (output > p->output_max) {output = p->output_max;}
-    if (output < p->output_min) {output = p->output_min;}
+    output = p_term + i_term + d_term;
+
+    // Output clamp
+    if (output > p->output_max) {
+        output = p->output_max;
+    }
+    else if (output < p->output_min) {
+        output = p->output_min;
+    }
 
     return output;
 }
+
+//int32_t Ctrl_PID_Update2_(CtrlPID_t *p){
+//    int32_t error;
+//    int32_t value_p;
+//    int32_t value_d;
+//    int32_t output;
+//
+//    // Calculate error
+//    error = p->sp - p->pv;
+//
+//    // Proportional term
+//    value_p = (p->kp * error) >> p->scale;
+//
+//    // Integral term accumulation
+//    p->value_i += (p->ki * error) >> p->scale;
+//
+//    // Anti-windup: limit integral term
+//    if (p->value_i > p->output_max) p->value_i = p->output_max;
+//    if (p->value_i < p->output_min) p->value_i = p->output_min;
+//
+//    // Derivative term
+//    value_d = (p->kd * (error - p->error_last)) >> p->scale;
+//
+//    // Store current error for next iteration
+//    p->error_last = error;
+//
+//    // Compute PID output
+//    output = value_p + p->value_i + value_d;
+//    if (output > p->output_max) {output = p->output_max;}
+//    if (output < p->output_min) {output = p->output_min;}
+//
+//    return output;
+//}
 
 ////******************************************
 //float Ctrl_PID_Update(float sp, float pv){
--- Modules/controller_pid.h
+++ Modules/controller_pid.h
@@ -9,13 +9,6 @@ extern "C" {
 
 #include <stdint.h>
 
-#ifndef CONTROLLER_PID_HARDWARE
-#define CONTROLLER_PID_HARDWARE
-    #define KP 5 //(0.5 * 10.23) // 5.115
-    #define KI 0 //(0.000005 * 10.23) // 0.00005115
-    #define KD 10 //(0.1 * 10.23)    // 1.023
-#endif
-
 typedef enum {
     PID_SCALE_1       = 0,   /**< 2^0 */
     PID_SCALE_2       = 1,   /**< 2^1 */
@@ -37,6 +30,19 @@ typedef enum {
     PID_SCALE_131072  = 17   /**< 2^17 */
 } PID_Scale_t;
 
+typedef enum {
+    PID_DT_1MS    = 0,   /**< 2^0 ms */
+    PID_DT_2MS    = 1,   /**< 2^1 ms */
+    PID_DT_4MS    = 2,   /**< 2^2 ms */
+    PID_DT_8MS    = 3,   /**< 2^3 ms */
+    PID_DT_16MS   = 4,   /**< 2^4 ms */
+    PID_DT_32MS   = 5,   /**< 2^5 ms */
+    PID_DT_64MS   = 6,   /**< 2^6 ms */
+    PID_DT_128MS  = 7,   /**< 2^7 ms */
+    PID_DT_256MS  = 8,   /**< 2^8 ms */
+    PID_DT_512MS  = 9    /**< 2^9 ms */
+} PID_Dt_t;
+
 typedef struct {
     const int32_t kp;
     const int32_t ki;
@@ -45,17 +51,25 @@ typedef struct {
     const int32_t output_min;
     const int32_t output_max;
     const PID_Scale_t scale;
-    const int32_t dt;
+    const PID_Dt_t dt;
 
-    int32_t value_i;
+    int32_t i_sum;
     int32_t error_last;
 
     int32_t sp;
     int32_t pv;
 } CtrlPID_t;
 
+#ifndef CONTROLLER_PID_HARDWARE_EXTERA
+#define CONTROLLER_PID_HARDWARE_EXTERA
+    #define PID_SCALE PID_SCALE_1024
+
+    //#warning "CONTROLLER_PID_HARDWARE_EXTERA is not defined; default configuration will be used."
+#endif
+
+#define PID_FLOAT_TO_Q(x, scale)    ((int32_t)((x) * (float)(1UL << (scale)) + 0.5f))
+
 int32_t Ctrl_PID_Update2(CtrlPID_t *p);
-//float Ctrl_PID_Update(float sp, float pv);
 
 #ifdef __cplusplus
 }
