1c2a1e300f913666026261c1a0c1e66fe1764d95
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-04-02 07:15:23 +0330

    Update ntc100k.h

--- Modules/ntc100k.h
+++ Modules/ntc100k.h
@@ -1,7 +1,16 @@
 // GitHub Account: GitHub.com/AliRezaJoodi
 
-#ifndef NTC_100K_INCLUDED
-#define NTC_100K_INCLUDED
+#ifndef NTC100K_INCLUDED
+#define NTC100K_INCLUDED
+
+#include <stdint.h>
+
+#ifndef NTC100K_HARDWARE_EXTERA
+#define NTC100K_HARDWARE_EXTERA
+    #define NTC100K_RESISTOR        100000UL    //ohm
+    #define NTC100K_CH              0
+    #warning "NTC100K_HARDWARE_EXTERA is not defined; default configuration will be used."
+#endif
 
 float NTC100K_ConvertOhmToTemp(float ohm);
 
