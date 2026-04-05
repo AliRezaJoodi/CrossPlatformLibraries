163f42363a6775a358d6d58c46658e0aa4ac72f0
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-04-06 00:21:34 +0330

    chore: update utility_array

---
+++ Utils/utility_array.c
@@ -0,0 +1,87 @@
+#include "utility_array.h"
+
+//*************************************************************
+uint16_t Array_Min_u16(const uint16_t *arr, uint16_t len){
+    uint16_t i;
+    uint16_t min = arr[0];
+
+    if (arr == NULL || len == 0){
+        return 0xFFFF;
+    }
+
+    for(i = 1; i < len; i++){
+        if(arr[i] < min){
+            min = arr[i];
+        }
+    }
+
+    return min;
+}
+
+//*************************************************************
+uint16_t Array_Max_u16(const uint16_t *arr, uint16_t len){
+    uint16_t i;
+    uint16_t max = arr[0];
+
+    if (arr == NULL || len == 0){
+        return 0xFFFF;
+    }
+
+    for(i = 1; i < len; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+
+    return max;
+}
+
+//*************************************************************
+uint16_t Array_Average_u16(const uint16_t *arr, uint16_t len){
+    uint32_t sum = 0;
+    uint16_t i;
+
+    if (arr == NULL || len == 0){
+        return 0xFFFF;
+    }
+
+    for(i = 0; i < len; i++){
+        sum += arr[i];
+    }
+
+    return (uint16_t)(sum / len);
+}
+
+//*************************************************************
+int16_t Array_Find_u16(const uint16_t *arr, uint16_t len, uint16_t value){
+    uint16_t i;
+
+    for(i = 0; i < len; i++){
+        if(arr[i] == value){
+            return (int16_t)i;
+        }
+    }
+
+    return -1;
+}
+
+//*************************************************************
+void Array_Reverse_u16(uint16_t *arr, uint16_t len){
+    uint16_t i;
+    uint16_t tmp;
+
+    for(i = 0; i < len / 2; i++){
+        tmp = arr[i];
+        arr[i] = arr[len - 1 - i];
+        arr[len - 1 - i] = tmp;
+    }
+}
+
+//*************************************************************
+void Array_Clear_u16(uint16_t *arr, uint16_t len){
+    uint16_t i;
+
+    for(i = 0; i < len; i++){
+        arr[i] = 0;
+    }
+}
