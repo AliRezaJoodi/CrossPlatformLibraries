844bc9518ffed35ba595ba5b2747460ab043405f
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-04-09 00:15:48 +0330

    chore: update "thermocouple_k"

--- Modules/thermocouple_k.h
+++ Modules/thermocouple_k.h
@@ -1,7 +1,9 @@
 // GitHub Account:  GitHub.com/AliRezaJoodi
 
-#ifndef TC_INCLUDED
-#define TC_INCLUDED
+#ifndef THERMOCOUPLE_INCLUDED
+#define THERMOCOUPLE_INCLUDED
+
+#include <stdint.h>
 
 #ifndef TC_HARDWARE
 #define TC_HARDWARE
@@ -9,8 +11,16 @@
     #define TC_GAIN         1/90
 #endif
 
+
+#define TC_K_TEMP_MIN     -270
+#define TC_K_TEMP_MAX     1370
+#define TC_K_ERROR        -1000
+
 int TC_ConvertMilliVoltToTemp(float mv);
 float TC_ConvertTempToMilliVolt(int temp);
+
+int32_t TC_ConvertMicroVoltToTemp(int32_t uv);
+int32_t TC_ConvertTempToMicroVolt(int32_t uv);
 
 #endif
 
