08ef38bb2a7e22fe09fc608d2def76b9bc86fbb8
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-04-12 20:13:29 +0330

    Update calendar_converter.c

--- Modules/calendar_converter.c
+++ Modules/calendar_converter.c
@@ -24,8 +24,7 @@ void Calendar_ConvertGregorianToJalaali(date_t *gc, date_t *jc){
     uint8_t t1 = 0;
     uint8_t t2 = 0;
 
-    k = gc->year % 4;
-
+    k = gc->year & 0x03;    // gc->year % 4
     if(k == 3){k = 2;};
     k *= 2;
     t1 = table_gregorian[k][gc->month - 1];
@@ -38,15 +37,24 @@ void Calendar_ConvertGregorianToJalaali(date_t *gc, date_t *jc){
         jc->year = gc->year + 79;
     }
 
-    jc->year = jc->year % 100;
+    //jc->year = jc->year % 100;
+    if (jc->year >= 100) jc->year -= 100;
 
     if(gc->day <= t1){
-       jc->day = gc->day + t2;
-       jc->month = (gc->month + 8) % 12 + 1;
+        jc->day = gc->day + t2;
+        //jc->month = (gc->month + 8) % 12 + 1;
+        jc->month = gc->month + 9;
+        if (jc->month > 12) {
+            jc->month -= 12;
+        }
     }
     else{
-       jc->day = gc->day - t1;
-       jc->month = (gc->month + 9) % 12 + 1;
+        jc->day = gc->day - t1;
+        //jc->month = (gc->month + 9) % 12 + 1;
+        jc->month = gc->month + 10;
+        if (jc->month > 12){
+            jc->month -= 12;
+        }
     }
 }
 
@@ -56,8 +64,7 @@ void Calendar_ConvertJalaaliToGregorian(date_t *jc, date_t *gc){
     uint8_t t1 = 0;
     uint8_t t2 = 0;
 
-    k = (jc->year + 100) % 4;
-
+    k = (jc->year + 100) & 0x03;    //  % 4
     if(k == 0){
         k = 2;
     }
@@ -69,21 +76,34 @@ void Calendar_ConvertJalaaliToGregorian(date_t *jc, date_t *gc){
     t2 = table_jalaali[k-1][jc->month - 1];
 
     if(jc->month < 10 || (jc->month == 10 && jc->day <= table_jalaali[k-2][jc->month-1])){
-        gc->year = (jc->year + 100) - 79;
+        //gc->year = (jc->year + 100) - 79;
+        gc->year = jc->year + 21;
     }
     else{
-        gc->year = (jc->year + 100) - 78;
+        //gc->year = (jc->year + 100) - 78;
+        gc->year = jc->year + 22;
     }
 
-    gc->year = gc->year % 100;
+    //gc->year = gc->year % 100;
+    if (gc->year >= 100){
+        gc->year -= 100;
+    }
 
     if(jc->day <= t1){
-       gc->day = jc->day + t2;
-       gc->month = (jc -> month + 1) % 12 + 1;
+        gc->day = jc->day + t2;
+        //gc->month = (jc -> month + 1) % 12 + 1;
+        gc->month = jc->month + 2;
+        if (gc->month > 12){
+            gc->month -= 12;
+        }
     }
     else{
-       gc->day = jc->day - t1;
-       gc->month = (jc->month + 2) % 12 + 1;
+        gc->day = jc->day - t1;
+        //gc->month = (jc->month + 2) % 12 + 1;
+        gc->month = jc->month + 3;
+        if (gc->month > 12){
+            gc->month -= 12;
+        }
     }
 }
 
