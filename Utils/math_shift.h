5881f900597719ccb3cf0d5663dd08c0a09767de
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-04-13 04:28:42 +0330

    Update math_shift.h

--- Utils/math_shift.h
+++ Utils/math_shift.h
@@ -1,5 +1,5 @@
 /*
- * @brief
+ * @brief   Fast multiply/divide/mod operations using bit shifting
  *
  * @author  AliReza Joodi
  * @see     https://github.com/AliRezaJoodi
@@ -47,6 +47,23 @@ extern "C" {
 #define DIV_16384(x)   ((x) >> 14)
 #define DIV_32768(x)   ((x) >> 15)
 #define DIV_65536(x)   ((x) >> 16)
+
+#define MOD_2(x)       ((x) & 0x01)
+#define MOD_4(x)       ((x) & 0x03)
+#define MOD_8(x)       ((x) & 0x07)
+#define MOD_16(x)      ((x) & 0x0F)
+#define MOD_32(x)      ((x) & 0x1F)
+#define MOD_64(x)      ((x) & 0x3F)
+#define MOD_128(x)     ((x) & 0x7F)
+#define MOD_256(x)     ((x) & 0xFF)
+#define MOD_512(x)     ((x) & 0x1FF)
+#define MOD_1024(x)    ((x) & 0x3FF)
+#define MOD_2048(x)    ((x) & 0x7FF)
+#define MOD_4096(x)    ((x) & 0xFFF)
+#define MOD_8192(x)    ((x) & 0x1FFF)
+#define MOD_16384(x)   ((x) & 0x3FFF)
+#define MOD_32768(x)   ((x) & 0x7FFF)
+#define MOD_65536(x)   ((x) & 0xFFFF)
 
 #ifdef __cplusplus
 }
