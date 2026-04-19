0ba5de49b1a1feb440a1a359b8ca9efbb3ef6ac3
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-04-20 00:41:04 +0330

    chore: update "crc8"

--- Utils/crc8.c
+++ Utils/crc8.c
@@ -1,16 +1,69 @@
 #include "crc8.h"
 
 //************************************************
-uint8_t CRC8_ATM(uint8_t crc, uint8_t data){
+static inline uint8_t Reflect8Bit(uint8_t x){
+    x = (x >> 4) | (x << 4);
+    x = ((x & 0xCC) >> 2) | ((x & 0x33) << 2);
+    x = ((x & 0xAA) >> 1) | ((x & 0x55) << 1);
 
+    return x;
+}
+
+//************************************************
+uint8_t CRC8_ATM(uint8_t crc, uint8_t data){
+    uint8_t i = 0;
+
+    crc ^= data;
+
+    for (i = 0; i < 8; ++i){
+        if ((crc & 0x80) == 0x80){
+            crc = (crc << 1) ^ 0x07;
+        }
+        else{
+            crc <<= 1;
+        }
+    }
+
+    return crc;
 }
 
 //************************************************
 uint8_t CRC8_Maxim(uint8_t crc, uint8_t data){
+    uint8_t i = 0;
 
+    crc  = Reflect8Bit(crc);
+    data = Reflect8Bit(data);
+
+    crc ^= data;
+
+    for (i = 0; i < 8; ++i){
+        if ((crc & 0x80) == 0x80){
+            crc = (crc << 1) ^ 0x31;
+        }
+        else{
+            crc <<= 1;
+        }
+    }
+
+    crc = Reflect8Bit(crc);
+
+    return crc;
 }
 
 //************************************************
 uint8_t CRC8_J1850(uint8_t crc, uint8_t data){
+    uint8_t i = 0;
 
+    crc ^= data;
+
+    for (i = 0; i < 8; ++i){
+        if ((crc & 0x80) == 0x80){
+            crc = (crc << 1) ^ 0x1D;
+        }
+        else{
+            crc <<= 1;
+        }
+    }
+
+    return crc;
 }
--- Utils/crc8.h
+++ Utils/crc8.h
@@ -1,3 +1,10 @@
+/*
+ * @brief CRC8 calculation functions. *
+ *
+ * @author  AliReza Joodi
+ * @see     https://github.com/AliRezaJoodi
+ */
+
 #ifndef CRC8_INCLUDED
 #define CRC8_INCLUDED
 
@@ -7,8 +14,62 @@ extern "C" {
 
 #include <stdint.h>
 
+/**
+ * @brief   Compute CRC-8/ATM over one byte (bitwise, non-reflected).
+ *
+ * @details Parameters of CRC-8/ATM:
+ *          - Polynomial: 0x07
+ *          - Init value: user provided (commonly 0x00)
+ *          - RefIn:      false
+ *          - RefOut:     false
+ *          - XorOut:     0x00
+ *
+ * @param   crc   Current CRC accumulator.
+ * @param   data  Input byte to be processed.
+ *
+ * @return  Updated CRC value.
+ *
+ * @note    Designed for incremental (streaming) CRC calculation:
+ *          crc = CRC8_ATM(crc, byte);
+ */
 uint8_t CRC8_ATM(uint8_t crc, uint8_t data);
+
+/**
+ * @brief   Compute CRC-8/MAXIM (Dallas 1-Wire) over one byte.
+ *
+ * @details Parameters of CRC-8/MAXIM:
+ *          - Polynomial: 0x31
+ *          - Init value: user provided (commonly 0x00)
+ *          - RefIn:      true
+ *          - RefOut:     true
+ *          - XorOut:     0x00
+ *
+ * @param   crc   Current CRC accumulator (will be bit-reflected internally).
+ * @param   data  Input byte to be processed (will be bit-reflected internally).
+ *
+ * @return  Updated CRC value (reflected output).
+ *
+ * @note    This is the standard Dallas/Maxim 1-Wire CRC (OWI CRC-8).
+ */
 uint8_t CRC8_Maxim(uint8_t crc, uint8_t data);
+
+/**
+ * @brief   Compute CRC-8/SAE J1850 over one byte (non-reflected).
+ *
+ * @details Parameters of CRC-8/J1850:
+ *          - Polynomial: 0x1D
+ *          - Init value: user provided (commonly 0xFF)
+ *          - RefIn:      false
+ *          - RefOut:     false
+ *          - XorOut:     0xFF
+ *
+ * @param   crc   Current CRC accumulator.
+ * @param   data  Input byte to be processed.
+ *
+ * @return  Updated CRC value (XorOut must be applied outside the function).
+ *
+ * @note    Final CRC is:  final_crc = CRC8_J1850(... ) ^ 0xFF
+ */
 uint8_t CRC8_J1850(uint8_t crc, uint8_t data);
 
 #ifdef __cplusplus
