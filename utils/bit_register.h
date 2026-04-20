ed57c57ae28be78e425f4436e118f8914f21f0e2
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-04-20 13:01:50 +0330

    Update bit_register.h

--- utils/bit_register.h
+++ utils/bit_register.h
@@ -9,11 +9,27 @@ extern "C" {
 
 #include <stdint.h>
 
+static inline void Set_Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+    *reg = (uint8_t)(*reg | (1U << pos));
+}
+
+static inline void Clear_Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+    *reg = (uint8_t)(*reg & ~(1U << pos));
+}
+
+static inline void Toggle_Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+    *reg = (uint8_t)(*reg ^ (1U << pos));
+}
+
 static inline void Write_Bit_Reg8(volatile uint8_t *reg, uint8_t pos, uint8_t status){
     *reg = (uint8_t)(
             (*reg & ~(1U << pos)) |
             ((status & 1U) << pos)
            );
+}
+
+static inline uint8_t Get_Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+    return (uint8_t)((*reg >> pos) & 0x01U);
 }
 
 #ifdef __cplusplus
