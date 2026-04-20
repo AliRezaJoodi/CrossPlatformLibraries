5eb942763f5857edcf8a9e577191d0bd880643c4
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-04-20 04:42:50 +0330

    Update utility_bit_register.h

--- Utils/utility_bit_register.h
+++ Utils/utility_bit_register.h
@@ -9,6 +9,12 @@ extern "C" {
 
 #include <stdint.h>
 
+static inline void Write_Bit_Reg8(volatile uint8_t *reg, uint8_t pos, uint8_t status){
+    *reg = (uint8_t)(
+            (*reg & ~(1U << pos)) |
+            ((status & 1U) << pos)
+           );
+}
 
 #ifdef __cplusplus
 }
