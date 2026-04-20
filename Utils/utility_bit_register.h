5c4c27a1cf64346c892e37e8d1e22506786a6bf3
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-04-20 04:34:27 +0330

    Create utility_bit_register.h

---
+++ Utils/utility_bit_register.h
@@ -0,0 +1,17 @@
+// GitHub Account: GitHub.com/AliRezaJoodi
+
+#ifndef UTILITY_BIT_REGISTER_INCLUDED
+#define UTILITY_BIT_REGISTER_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
