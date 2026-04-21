e88d28636f210ecefb09648f4b1bc6033d84517a
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-04-21 14:18:55 +0330

    Update bit_register.h

--- utils/bit_register.h
+++ utils/bit_register.h
@@ -10,155 +10,155 @@ extern "C" {
 #include <stdint.h>
 
 //***********************************************************************
-static inline void Set_Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline void SetBit_Reg8(volatile uint8_t *reg, uint8_t pos){
     *reg = (uint8_t)(*reg | (0x01U << pos));
 }
 
-static inline void Set_2Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline void Set2Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
     *reg = (uint8_t)(*reg | (0x03U << pos));
 }
 
-static inline void Set_3Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline void Set3Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
     *reg = (uint8_t)(*reg | (0x07U << pos));
 }
 
-static inline void Set_4Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline void Set4Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
     *reg = (uint8_t)(*reg | (0x0FU << pos));
 }
 
-static inline void Set_5Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline void Set5Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
     *reg = (uint8_t)(*reg | (0x1FU << pos));
 }
 
-static inline void Set_6Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline void Set6Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
     *reg = (uint8_t)(*reg | (0x3FU << pos));
 }
 
-static inline void Set_7Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline void Set7Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
     *reg = (uint8_t)(*reg | (0x7FU << pos));
 }
 
-static inline void Set_8Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline void Set8Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
     *reg = (uint8_t)(*reg | (0xFFU << pos));
 }
 
 //***********************************************************************
-static inline void Clear_Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline void ClearBit_Reg8(volatile uint8_t *reg, uint8_t pos){
     *reg = (uint8_t)(*reg & ~(0x01U << pos));
 }
 
-static inline void Clear_2Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline void Clear2Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
     *reg = (uint8_t)(*reg & ~(0x03U << pos));
 }
 
-static inline void Clear_3Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline void Clear3Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
     *reg = (uint8_t)(*reg & ~(0x07U << pos));
 }
 
-static inline void Clear_4Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline void Clear4Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
     *reg = (uint8_t)(*reg & ~(0x0FU << pos));
 }
 
-static inline void Clear_5Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline void Clear5Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
     *reg = (uint8_t)(*reg & ~(0x1FU << pos));
 }
 
-static inline void Clear_6Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline void Clear6Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
     *reg = (uint8_t)(*reg & ~(0x3FU << pos));
 }
 
-static inline void Clear_7Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline void Clear7Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
     *reg = (uint8_t)(*reg & ~(0x7FU << pos));
 }
 
-static inline void Clear_8Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline void Clear8Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
     *reg = (uint8_t)(*reg & ~(0xFFU << pos));
 }
 
 //***********************************************************************
-static inline void Toggle_Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline void ToggleBit_Reg8(volatile uint8_t *reg, uint8_t pos){
     *reg = (uint8_t)(*reg ^ (0x01U << pos));
 }
 
-static inline void Toggle_2Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline void Toggle2Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
     *reg = (uint8_t)(*reg ^ (0x03U << pos));
 }
 
-static inline void Toggle_3Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline void Toggle3Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
     *reg = (uint8_t)(*reg ^ (0x07U << pos));
 }
 
-static inline void Toggle_4Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline void Toggle4Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
     *reg = (uint8_t)(*reg ^ (0x0FU << pos));
 }
 
-static inline void Toggle_5Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline void Toggle5Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
     *reg = (uint8_t)(*reg ^ (0x1FU << pos));
 }
 
-static inline void Toggle_6Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline void Toggle6Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
     *reg = (uint8_t)(*reg ^ (0x3FU << pos));
 }
 
-static inline void Toggle_7Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline void Toggle7Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
     *reg = (uint8_t)(*reg ^ (0x7FU << pos));
 }
 
-static inline void Toggle_8Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline void Toggle8Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
     *reg = (uint8_t)(*reg ^ (0xFFU << pos));
 }
 
 //***********************************************************************
-static inline void Write_Bit_Reg8(volatile uint8_t *reg, uint8_t pos, uint8_t status){
+static inline void WriteBit_Reg8(volatile uint8_t *reg, uint8_t pos, uint8_t status){
     *reg = (uint8_t)(
             (*reg & ~(0x01U << pos)) |
             ((status & 0x01U) << pos)
            );
 }
 
-static inline void Write_2Bit_Reg8(volatile uint8_t *reg, uint8_t pos, uint8_t status){
+static inline void Write2Bit_Reg8(volatile uint8_t *reg, uint8_t pos, uint8_t status){
     *reg = (uint8_t)(
             (*reg & ~(0x03U << pos)) |
             ((status & 0x03U) << pos)
            );
 }
 
-static inline void Write_3Bit_Reg8(volatile uint8_t *reg, uint8_t pos, uint8_t status){
+static inline void Write3Bit_Reg8(volatile uint8_t *reg, uint8_t pos, uint8_t status){
     *reg = (uint8_t)(
             (*reg & ~(0x07U << pos)) |
             ((status & 0x07U) << pos)
            );
 }
 
-static inline void Write_4Bit_Reg8(volatile uint8_t *reg, uint8_t pos, uint8_t status){
+static inline void Write4Bit_Reg8(volatile uint8_t *reg, uint8_t pos, uint8_t status){
     *reg = (uint8_t)(
             (*reg & ~(0x0FU << pos)) |
             ((status & 0x0FU) << pos)
            );
 }
 
-static inline void Write_5Bit_Reg8(volatile uint8_t *reg, uint8_t pos, uint8_t status){
+static inline void Write5Bit_Reg8(volatile uint8_t *reg, uint8_t pos, uint8_t status){
     *reg = (uint8_t)(
             (*reg & ~(0x1FU << pos)) |
             ((status & 0x1FU) << pos)
            );
 }
 
-static inline void Write_6Bit_Reg8(volatile uint8_t *reg, uint8_t pos, uint8_t status){
+static inline void Write6Bit_Reg8(volatile uint8_t *reg, uint8_t pos, uint8_t status){
     *reg = (uint8_t)(
             (*reg & ~(0x3FU << pos)) |
             ((status & 0x3FU) << pos)
            );
 }
 
-static inline void Write_7Bit_Reg8(volatile uint8_t *reg, uint8_t pos, uint8_t status){
+static inline void Write7Bit_Reg8(volatile uint8_t *reg, uint8_t pos, uint8_t status){
     *reg = (uint8_t)(
             (*reg & ~(0x7FU << pos)) |
             ((status & 0x7FU) << pos)
            );
 }
 
-static inline void Write_8Bit_Reg8(volatile uint8_t *reg, uint8_t pos, uint8_t status){
+static inline void Write8Bit_Reg8(volatile uint8_t *reg, uint8_t pos, uint8_t status){
     *reg = (uint8_t)(
             (*reg & ~(0xFFU << pos)) |
             ((status & 0xFFU) << pos)
@@ -166,35 +166,35 @@ static inline void Write_8Bit_Reg8(volatile uint8_t *reg, uint8_t pos, uint8_t s
 }
 
 //***********************************************************************
-static inline uint8_t Get_Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline uint8_t GetBit_Reg8(volatile uint8_t *reg, uint8_t pos){
     return (uint8_t)((*reg >> pos) & 0x01U);
 }
 
-static inline uint8_t Get_2Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline uint8_t Get2Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
     return (uint8_t)((*reg >> pos) & 0x03U);
 }
 
-static inline uint8_t Get_3Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline uint8_t Get3Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
     return (uint8_t)((*reg >> pos) & 0x07U);
 }
 
-static inline uint8_t Get_4Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline uint8_t Get4Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
     return (uint8_t)((*reg >> pos) & 0x0FU);
 }
 
-static inline uint8_t Get_5Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline uint8_t Get5Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
     return (uint8_t)((*reg >> pos) & 0x1FU);
 }
 
-static inline uint8_t Get_6Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline uint8_t Get6Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
     return (uint8_t)((*reg >> pos) & 0x3FU);
 }
 
-static inline uint8_t Get_7Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline uint8_t Get7Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
     return (uint8_t)((*reg >> pos) & 0x7FU);
 }
 
-static inline uint8_t Get_8Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+static inline uint8_t Get8Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
     return (uint8_t)((*reg >> pos) & 0xFFU);
 }
 
