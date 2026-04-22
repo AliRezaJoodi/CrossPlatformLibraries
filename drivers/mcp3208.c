ca8ebf0933845e3f9bbdabfee5ad0d1d5f558e86
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-04-23 01:11:07 +0330

    chore: update "mcp3208"

--- drivers/mcp3208.c
+++ drivers/mcp3208.c
@@ -4,10 +4,13 @@
 #include "mcp3208_port.h"
 #include "drivers/mcp3208.h"
 
+#define CS_IDLE             1U
+#define CS_ACTIVE           0U
+
 /********************************************************/
 void MCP3208_Init(MCP3208_t *mcp){
     MCP3208_CS_InitPin(mcp);
-    MCP3208_CS_WritePin(mcp, 1);    // Idle bus
+    MCP3208_CS_WritePin(mcp, CS_IDLE);
 }
 
 /********************************************************/
@@ -84,11 +87,11 @@ uint16_t MCP3208_GetCounts(MCP3208_t *mcp, MCP3208_Channel_t ch){
             return 0xFFFF;
     }
 
-    MCP3208_CS_WritePin(mcp, 0);
+    MCP3208_CS_WritePin(mcp, CS_ACTIVE);
     MCP3208_SPI_Transfer(data1);
     data1 = MCP3208_SPI_Transfer(data2);    // Get MSB
     data2 = MCP3208_SPI_Transfer(0xFF);     // Get LSB
-    MCP3208_CS_WritePin(mcp, 1);
+    MCP3208_CS_WritePin(mcp, CS_IDLE);
 
     return ( ((uint16_t)(data1 & 0x0FU) << 8U) | data2 );
 }
