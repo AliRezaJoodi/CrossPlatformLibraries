dec81e0dd3d3d012536c1033b47be35bc3c36016
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-04-23 05:29:34 +0330

    Update ntc100k.c

--- modules/ntc100k.c
+++ modules/ntc100k.c
@@ -1,6 +1,7 @@
 // GitHub Account: GitHub.com/AliRezaJoodi
 
-#include "ntc100k.h"
+#include <stdint.h>
+#include "modules/ntc100k.h"
 
 const uint32_t ntc100k_table5c[] = {
     10723236UL, // -55°C
