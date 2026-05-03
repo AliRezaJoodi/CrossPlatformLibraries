51596d460793c8df5fae110e3384c11fad775f83
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-05-03 05:06:42 +0330

    chore: update "mcp3208"

--- drivers/mcp3208.c
+++ drivers/mcp3208.c
@@ -9,9 +9,9 @@
 
 /********************************************************/
 void MCP3208_Init(MCP3208_t *mcp){
-    MCP3208_CS_SetOutput(mcp);
+    MCP3208_CS_ConfigOutput(mcp);
     //MCP3208_CS_Write(mcp, CS_IDLE);
-    MCP3208_CS_WriteHigh(mcp);
+    MCP3208_CS_Set(mcp);
 }
 
 /********************************************************/
@@ -89,12 +89,12 @@ uint16_t MCP3208_GetCounts(MCP3208_t *mcp, MCP3208_Channel_t ch){
     }
 
     //MCP3208_CS_Write(mcp, CS_ACTIVE);
-    MCP3208_CS_WriteLow(mcp);
+    MCP3208_CS_Clear(mcp);
     MCP3208_SPI_Transfer(data1);
     data1 = MCP3208_SPI_Transfer(data2);    // Get MSB
     data2 = MCP3208_SPI_Transfer(0xFF);     // Get LSB
     //MCP3208_CS_Write(mcp, CS_IDLE);
-    MCP3208_CS_WriteHigh(mcp);
+    MCP3208_CS_Set(mcp);
 
     return ( ((uint16_t)(data1 & 0x0FU) << 8U) | data2 );
 }
--- platforms/avr/mcp3208_port.h
+++ platforms/avr/mcp3208_port.h
@@ -57,7 +57,7 @@ extern "C" {
  *
  * @param   mcp     Pointer to the MCP3208 instance
  */
-static inline void MCP3208_CS_SetOutput(MCP3208_t *mcp){
+static inline void MCP3208_CS_ConfigOutput(const MCP3208_t *mcp){
     SetBitMask_Reg8(mcp->cs.ddr, mcp->cs.mask);
 }
 
@@ -69,17 +69,17 @@ static inline void MCP3208_CS_SetOutput(MCP3208_t *mcp){
  *                  - 0: Pull CS low (select chip)
  *                  - 1: Pull CS high (deselect / idle)
  *
- * @note    The CS pin must be initialized with MCP3208_CS_SetOutput() before use.
+ * @note    The CS pin must be initialized with MCP3208_CS_ConfigOutput() before use.
  */
 //static inline void MCP3208_CS_Write(MCP3208_t *mcp, uint8_t status){
 //    WriteBit_Reg8(mcp->cs.port, mcp->cs.index, status);
 //}
 
-static inline void MCP3208_CS_WriteLow(MCP3208_t *mcp){
+static inline void MCP3208_CS_Clear(const MCP3208_t *mcp){
     ClearBitMask_Reg8(mcp->cs.port, mcp->cs.mask);
 }
 
-static inline void MCP3208_CS_WriteHigh(MCP3208_t *mcp){
+static inline void MCP3208_CS_Set(const MCP3208_t *mcp){
     SetBitMask_Reg8(mcp->cs.port, mcp->cs.mask);
 }
 
@@ -92,7 +92,7 @@ static inline void MCP3208_CS_WriteHigh(MCP3208_t *mcp){
  * @note    The SPI peripheral must be configured and enabled
  *          before calling this function.
  */
-static inline uint8_t MCP3208_SPI_Transfer(uint8_t data){
+static inline uint8_t MCP3208_SPI_Transfer(const uint8_t data){
     uint16_t timeout = 1000U;           /* Software timeout counter */
 
     SPDR = data;                        /* Start SPI transfer */
