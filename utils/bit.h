50668ca79f033441920dab7f7d8815a990030996
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-05-05 05:30:35 +0330

    Create bit.h

---
+++ utils/bit.h
@@ -0,0 +1,19 @@
+// GitHub Account: GitHub.com/AliRezaJoodi
+
+#ifndef BIT_INCLUDED
+#define BIT_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+#include "utils/bit_macro.h"
+#include "utils/bit_register.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
