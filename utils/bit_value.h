274ce87205f8bfda3b1dea71af0c42d6e5257ac8
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-05-05 14:26:52 +0330

    Create bit_value.h

---
+++ utils/bit_value.h
@@ -0,0 +1,19 @@
+// GitHub Account: GitHub.com/AliRezaJoodi
+
+#ifndef BIT_VALUE_INCLUDED
+#define BIT_VALUE_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+#include "utils/bit_value8.h"
+#include "utils/bit_value32.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
