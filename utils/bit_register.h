52115ebec6f522bac4f1c4e1b23ac7f2a069dfb2
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-05-05 14:09:03 +0330

    Create bit_register.h

---
+++ utils/bit_register.h
@@ -0,0 +1,19 @@
+// GitHub Account: GitHub.com/AliRezaJoodi
+
+#ifndef BIT_REGISTER_INCLUDED
+#define BIT_REGISTER_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+#include "utils/bit_register8.h"
+#include "utils/bit_register32.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
