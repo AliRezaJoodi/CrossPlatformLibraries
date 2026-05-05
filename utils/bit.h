b1ee7462765361329a5fc35a709ef4ffd72efb8d
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-05-05 14:26:40 +0330

    Update bit.h

--- utils/bit.h
+++ utils/bit.h
@@ -10,7 +10,10 @@ extern "C" {
 #include <stdint.h>
 
 #include "utils/bit_macro.h"
-#include "utils/bit_register.h"
+#include "utils/bit_register8.h"
+#include "utils/bit_register32.h"
+#include "utils/bit_value8.h"
+#include "utils/bit_value32.h"
 
 #ifdef __cplusplus
 }
