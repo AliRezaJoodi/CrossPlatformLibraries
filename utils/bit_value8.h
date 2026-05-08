cb3a78f29ec7ef2549d3fe6aa8abc8e7487ef852
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-05-09 02:59:26 +0330

    Update bit_value8.h

--- utils/bit_value8.h
+++ utils/bit_value8.h
@@ -10,103 +10,115 @@ extern "C" {
 #include <stdint.h>
 
 //***********************************************************************
+static inline uint8_t SetBitMask_u8(uint8_t value, uint8_t mask){
+    return (uint8_t)(value | (mask));
+}
+
 static inline uint8_t SetBit_u8(uint8_t value, uint8_t pos){
     return (uint8_t)(value | (0x01U << pos));
 }
 
-static inline uint8_t Set2Bit_u8(uint8_t value, uint8_t pos){
-    return (uint8_t)(value | ((0x03U) << pos));
-}
-
-static inline uint8_t Set3Bit_u8(uint8_t value, uint8_t pos){
-    return (uint8_t)(value | ((0x07U) << pos));
-}
-
-static inline uint8_t Set4Bit_u8(uint8_t value, uint8_t pos){
-    return (uint8_t)(value | ((0x0FU) << pos));
-}
-
-static inline uint8_t Set5Bit_u8(uint8_t value, uint8_t pos){
-    return (uint8_t)(value | ((0x1FU) << pos));
-}
-
-static inline uint8_t Set6Bit_u8(uint8_t value, uint8_t pos){
-    return (uint8_t)(value | ((0x3FU) << pos));
-}
-
-static inline uint8_t Set7Bit_u8(uint8_t value, uint8_t pos){
-    return (uint8_t)(value | ((0x7FU) << pos));
-}
-
-static inline uint8_t Set8Bit_u8(uint8_t value, uint8_t pos){
-    return (uint8_t)(value | ((0xFFU) << pos));
-}
+//static inline uint8_t Set2Bit_u8(uint8_t value, uint8_t pos){
+//    return (uint8_t)(value | ((0x03U) << pos));
+//}
+//
+//static inline uint8_t Set3Bit_u8(uint8_t value, uint8_t pos){
+//    return (uint8_t)(value | ((0x07U) << pos));
+//}
+//
+//static inline uint8_t Set4Bit_u8(uint8_t value, uint8_t pos){
+//    return (uint8_t)(value | ((0x0FU) << pos));
+//}
+//
+//static inline uint8_t Set5Bit_u8(uint8_t value, uint8_t pos){
+//    return (uint8_t)(value | ((0x1FU) << pos));
+//}
+//
+//static inline uint8_t Set6Bit_u8(uint8_t value, uint8_t pos){
+//    return (uint8_t)(value | ((0x3FU) << pos));
+//}
+//
+//static inline uint8_t Set7Bit_u8(uint8_t value, uint8_t pos){
+//    return (uint8_t)(value | ((0x7FU) << pos));
+//}
+//
+//static inline uint8_t Set8Bit_u8(uint8_t value, uint8_t pos){
+//    return (uint8_t)(value | ((0xFFU) << pos));
+//}
 
 //***********************************************************************
+static inline uint8_t ClearBitMask_u8(uint8_t value, uint8_t mask){
+    return (uint8_t)(value & ~(mask));
+}
+
 static inline uint8_t ClearBit_u8(uint8_t value, uint8_t pos){
     return (uint8_t)(value & ~(0x01U << pos));
 }
 
-static inline uint8_t Clear2Bit_u8(uint8_t value, uint8_t pos){
-    return (uint8_t)(value & ~((0x03U) << pos));
-}
-
-static inline uint8_t Clear3Bit_u8(uint8_t value, uint8_t pos){
-    return (uint8_t)(value & ~((0x07U) << pos));
-}
-
-static inline uint8_t Clear4Bit_u8(uint8_t value, uint8_t pos){
-    return (uint8_t)(value & ~((0x0FU) << pos));
-}
-
-static inline uint8_t Clear5Bit_u8(uint8_t value, uint8_t pos){
-    return (uint8_t)(value & ~((0x1FU) << pos));
-}
-
-static inline uint8_t Clear6Bit_u8(uint8_t value, uint8_t pos){
-    return (uint8_t)(value & ~((0x3FU) << pos));
-}
-
-static inline uint8_t Clear7Bit_u8(uint8_t value, uint8_t pos){
-    return (uint8_t)(value & ~((0x7FU) << pos));
-}
-
-static inline uint8_t Clear8Bit_u8(uint8_t value, uint8_t pos){
-    return (uint8_t)(value & ~((0xFFU) << pos));
-}
+//static inline uint8_t Clear2Bit_u8(uint8_t value, uint8_t pos){
+//    return (uint8_t)(value & ~((0x03U) << pos));
+//}
+//
+//static inline uint8_t Clear3Bit_u8(uint8_t value, uint8_t pos){
+//    return (uint8_t)(value & ~((0x07U) << pos));
+//}
+//
+//static inline uint8_t Clear4Bit_u8(uint8_t value, uint8_t pos){
+//    return (uint8_t)(value & ~((0x0FU) << pos));
+//}
+//
+//static inline uint8_t Clear5Bit_u8(uint8_t value, uint8_t pos){
+//    return (uint8_t)(value & ~((0x1FU) << pos));
+//}
+//
+//static inline uint8_t Clear6Bit_u8(uint8_t value, uint8_t pos){
+//    return (uint8_t)(value & ~((0x3FU) << pos));
+//}
+//
+//static inline uint8_t Clear7Bit_u8(uint8_t value, uint8_t pos){
+//    return (uint8_t)(value & ~((0x7FU) << pos));
+//}
+//
+//static inline uint8_t Clear8Bit_u8(uint8_t value, uint8_t pos){
+//    return (uint8_t)(value & ~((0xFFU) << pos));
+//}
 
 //***********************************************************************
+static inline uint8_t ToggleBitMask_u8(uint8_t value, uint8_t mask){
+    return (uint8_t)(value ^ (mask));
+}
+
 static inline uint8_t ToggleBit_u8(uint8_t value, uint8_t pos){
     return (uint8_t)(value ^ (0x01U << pos));
 }
 
-static inline uint8_t Toggle2Bit_u8(uint8_t value, uint8_t pos){
-    return (uint8_t)(value ^ ((0x03U) << pos));
-}
-
-static inline uint8_t Toggle3Bit_u8(uint8_t value, uint8_t pos){
-    return (uint8_t)(value ^ ((0x07U) << pos));
-}
-
-static inline uint8_t Toggle4Bit_u8(uint8_t value, uint8_t pos){
-    return (uint8_t)(value ^ ((0x0FU) << pos));
-}
-
-static inline uint8_t Toggle5Bit_u8(uint8_t value, uint8_t pos){
-    return (uint8_t)(value ^ ((0x1FU) << pos));
-}
-
-static inline uint8_t Toggle6Bit_u8(uint8_t value, uint8_t pos){
-    return (uint8_t)(value ^ ((0x3FU) << pos));
-}
-
-static inline uint8_t Toggle7Bit_u8(uint8_t value, uint8_t pos){
-    return (uint8_t)(value ^ ((0x7FU) << pos));
-}
-
-static inline uint8_t Toggle8Bit_u8(uint8_t value, uint8_t pos){
-    return (uint8_t)(value ^ ((0xFFU) << pos));
-}
+//static inline uint8_t Toggle2Bit_u8(uint8_t value, uint8_t pos){
+//    return (uint8_t)(value ^ ((0x03U) << pos));
+//}
+//
+//static inline uint8_t Toggle3Bit_u8(uint8_t value, uint8_t pos){
+//    return (uint8_t)(value ^ ((0x07U) << pos));
+//}
+//
+//static inline uint8_t Toggle4Bit_u8(uint8_t value, uint8_t pos){
+//    return (uint8_t)(value ^ ((0x0FU) << pos));
+//}
+//
+//static inline uint8_t Toggle5Bit_u8(uint8_t value, uint8_t pos){
+//    return (uint8_t)(value ^ ((0x1FU) << pos));
+//}
+//
+//static inline uint8_t Toggle6Bit_u8(uint8_t value, uint8_t pos){
+//    return (uint8_t)(value ^ ((0x3FU) << pos));
+//}
+//
+//static inline uint8_t Toggle7Bit_u8(uint8_t value, uint8_t pos){
+//    return (uint8_t)(value ^ ((0x7FU) << pos));
+//}
+//
+//static inline uint8_t Toggle8Bit_u8(uint8_t value, uint8_t pos){
+//    return (uint8_t)(value ^ ((0xFFU) << pos));
+//}
 
 //***********************************************************************
 static inline uint8_t WriteBit_u8(uint8_t value, uint8_t pos, uint8_t status){
