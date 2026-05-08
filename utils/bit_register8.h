abbac97578262860a4c507687e2205de83a01a82
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-05-09 02:59:12 +0330

    Update bit_register8.h

--- utils/bit_register8.h
+++ utils/bit_register8.h
@@ -14,114 +14,115 @@ static inline void SetBitMask_Reg8(volatile uint8_t *reg, uint8_t mask){
     *reg = (uint8_t)(*reg | mask);
 }
 
-//***********************************************************************
 static inline void SetBit_Reg8(volatile uint8_t *reg, uint8_t pos){
     *reg = (uint8_t)(*reg | (0x01U << pos));
 }
 
-static inline void Set2Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
-    *reg = (uint8_t)(*reg | (0x03U << pos));
-}
-
-static inline void Set3Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
-    *reg = (uint8_t)(*reg | (0x07U << pos));
-}
-
-static inline void Set4Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
-    *reg = (uint8_t)(*reg | (0x0FU << pos));
-}
-
-static inline void Set5Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
-    *reg = (uint8_t)(*reg | (0x1FU << pos));
-}
-
-static inline void Set6Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
-    *reg = (uint8_t)(*reg | (0x3FU << pos));
-}
-
-static inline void Set7Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
-    *reg = (uint8_t)(*reg | (0x7FU << pos));
-}
-
-static inline void Set8Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
-    *reg = (uint8_t)(*reg | (0xFFU << pos));
-}
+//static inline void Set2Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+//    *reg = (uint8_t)(*reg | (0x03U << pos));
+//}
+//
+//static inline void Set3Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+//    *reg = (uint8_t)(*reg | (0x07U << pos));
+//}
+//
+//static inline void Set4Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+//    *reg = (uint8_t)(*reg | (0x0FU << pos));
+//}
+//
+//static inline void Set5Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+//    *reg = (uint8_t)(*reg | (0x1FU << pos));
+//}
+//
+//static inline void Set6Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+//    *reg = (uint8_t)(*reg | (0x3FU << pos));
+//}
+//
+//static inline void Set7Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+//    *reg = (uint8_t)(*reg | (0x7FU << pos));
+//}
+//
+//static inline void Set8Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+//    *reg = (uint8_t)(*reg | (0xFFU << pos));
+//}
 
 //***********************************************************************
 static inline void ClearBitMask_Reg8(volatile uint8_t *reg, uint8_t mask){
     *reg = (uint8_t)(*reg & (~mask));
 }
 
-//***********************************************************************
 static inline void ClearBit_Reg8(volatile uint8_t *reg, uint8_t pos){
     *reg = (uint8_t)(*reg & ~(0x01U << pos));
 }
 
-static inline void Clear2Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
-    *reg = (uint8_t)(*reg & ~(0x03U << pos));
-}
-
-static inline void Clear3Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
-    *reg = (uint8_t)(*reg & ~(0x07U << pos));
-}
-
-static inline void Clear4Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
-    *reg = (uint8_t)(*reg & ~(0x0FU << pos));
-}
-
-static inline void Clear5Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
-    *reg = (uint8_t)(*reg & ~(0x1FU << pos));
-}
-
-static inline void Clear6Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
-    *reg = (uint8_t)(*reg & ~(0x3FU << pos));
-}
-
-static inline void Clear7Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
-    *reg = (uint8_t)(*reg & ~(0x7FU << pos));
-}
-
-static inline void Clear8Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
-    *reg = (uint8_t)(*reg & ~(0xFFU << pos));
-}
+//static inline void Clear2Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+//    *reg = (uint8_t)(*reg & ~(0x03U << pos));
+//}
+//
+//static inline void Clear3Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+//    *reg = (uint8_t)(*reg & ~(0x07U << pos));
+//}
+//
+//static inline void Clear4Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+//    *reg = (uint8_t)(*reg & ~(0x0FU << pos));
+//}
+//
+//static inline void Clear5Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+//    *reg = (uint8_t)(*reg & ~(0x1FU << pos));
+//}
+//
+//static inline void Clear6Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+//    *reg = (uint8_t)(*reg & ~(0x3FU << pos));
+//}
+//
+//static inline void Clear7Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+//    *reg = (uint8_t)(*reg & ~(0x7FU << pos));
+//}
+//
+//static inline void Clear8Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+//    *reg = (uint8_t)(*reg & ~(0xFFU << pos));
+//}
 
 //***********************************************************************
 static inline void ToggleBitMask_Reg8(volatile uint8_t *reg, uint8_t mask){
     *reg = (uint8_t)(*reg ^ mask);
 }
 
-//***********************************************************************
 static inline void ToggleBit_Reg8(volatile uint8_t *reg, uint8_t pos){
     *reg = (uint8_t)(*reg ^ (0x01U << pos));
 }
 
-static inline void Toggle2Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
-    *reg = (uint8_t)(*reg ^ (0x03U << pos));
+static inline void TogglePort_Reg8(volatile uint8_t *reg){
+    *reg = (uint8_t)(*reg ^ 0xFFU);
 }
 
-static inline void Toggle3Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
-    *reg = (uint8_t)(*reg ^ (0x07U << pos));
-}
-
-static inline void Toggle4Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
-    *reg = (uint8_t)(*reg ^ (0x0FU << pos));
-}
-
-static inline void Toggle5Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
-    *reg = (uint8_t)(*reg ^ (0x1FU << pos));
-}
-
-static inline void Toggle6Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
-    *reg = (uint8_t)(*reg ^ (0x3FU << pos));
-}
-
-static inline void Toggle7Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
-    *reg = (uint8_t)(*reg ^ (0x7FU << pos));
-}
-
-static inline void Toggle8Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
-    *reg = (uint8_t)(*reg ^ (0xFFU << pos));
-}
+//static inline void Toggle2Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+//    *reg = (uint8_t)(*reg ^ (0x03U << pos));
+//}
+//
+//static inline void Toggle3Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+//    *reg = (uint8_t)(*reg ^ (0x07U << pos));
+//}
+//
+//static inline void Toggle4Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+//    *reg = (uint8_t)(*reg ^ (0x0FU << pos));
+//}
+//
+//static inline void Toggle5Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+//    *reg = (uint8_t)(*reg ^ (0x1FU << pos));
+//}
+//
+//static inline void Toggle6Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+//    *reg = (uint8_t)(*reg ^ (0x3FU << pos));
+//}
+//
+//static inline void Toggle7Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+//    *reg = (uint8_t)(*reg ^ (0x7FU << pos));
+//}
+//
+//static inline void Toggle8Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
+//    *reg = (uint8_t)(*reg ^ (0xFFU << pos));
+//}
 
 // Note: This branchless bit-mask technique may not work correctly with CodeVisionAVR due to compiler optimization issues.
 //static inline void WriteBitMask_Reg8(volatile uint8_t *reg, uint8_t mask, uint8_t status){
@@ -178,19 +179,15 @@ static inline void Write7Bit_Reg8(volatile uint8_t *reg, uint8_t pos, uint8_t st
            );
 }
 
-static inline void Write8Bit_Reg8(volatile uint8_t *reg, uint8_t pos, uint8_t status){
-    *reg = (uint8_t)(
-            (*reg & ~(0xFFU << pos)) |
-            ((status & 0xFFU) << pos)
-           );
+static inline void WritePort_Reg8(volatile uint8_t *reg, uint8_t value){
+    *reg = value;
 }
 
 //***********************************************************************
 static inline uint8_t ReadBitMask_Reg8(volatile uint8_t *reg, uint8_t mask){
-    return (uint8_t)(((*reg) & mask) != 0U);
+    return (uint8_t)((*reg & mask) != 0U);
 }
 
-//***********************************************************************
 static inline uint8_t ReadBit_Reg8(volatile uint8_t *reg, uint8_t pos){
     return (uint8_t)((*reg >> pos) & 0x01U);
 }
@@ -217,10 +214,6 @@ static inline uint8_t Read6Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
 
 static inline uint8_t Read7Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
     return (uint8_t)((*reg >> pos) & 0x7FU);
-}
-
-static inline uint8_t Read8Bit_Reg8(volatile uint8_t *reg, uint8_t pos){
-    return (uint8_t)((*reg >> pos) & 0xFFU);
 }
 
 #ifdef __cplusplus
