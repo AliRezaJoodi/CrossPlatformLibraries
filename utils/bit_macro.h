5e4ecc326fcfd957a13931cce2cfd26a83b55125
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-05-11 23:28:34 +0330

    Update bit_macro.h

--- utils/bit_macro.h
+++ utils/bit_macro.h
@@ -7,60 +7,43 @@
 extern "C" {
 #endif
 
-#define SET_BITMASK(ADDRESS, MASK)          ((ADDRESS) |= (MASK))
-#define SET_BIT(ADDRESS, POS)               ((ADDRESS) |= (0x01UL << (POS)))
+#define BM_SET_BITMASK(ADDRESS, MASK)          ((ADDRESS) |= (MASK))
+#define BM_SET_BIT(ADDRESS, POS)               ((ADDRESS) |= (0x01UL << (POS)))
 
-#define SET_BITMASK_DIRECT(ADDRESS, MASK)   ((ADDRESS) = (MASK))
-#define SET_BIT_DIRECT(ADDRESS, POS)        ((ADDRESS)  = (0x01UL << (POS)))
-//#define SET_2BIT(ADDRESS, POS)              ((ADDRESS) |= (0x03UL << (POS)))
-//#define SET_3BIT(ADDRESS, POS)              ((ADDRESS) |= (0x07UL << (POS)))
-//#define SET_4BIT(ADDRESS, POS)              ((ADDRESS) |= (0x0FUL << (POS)))
-//#define SET_5BIT(ADDRESS, POS)              ((ADDRESS) |= (0x1FUL << (POS)))
-//#define SET_6BIT(ADDRESS, POS)              ((ADDRESS) |= (0x3FUL << (POS)))
-//#define SET_7BIT(ADDRESS, POS)              ((ADDRESS) |= (0x7FUL << (POS)))
-//#define SET_8BIT(ADDRESS, POS)              ((ADDRESS) |= (0xFFUL << (POS)))
+#define BM_CLEAR_BITMASK(ADDRESS, MASK)        ((ADDRESS) &= ~(MASK))
+#define BM_CLEAR_BIT(ADDRESS, POS)             ((ADDRESS) &= ~(0x01UL << (POS)))
 
-#define CLEAR_BITMASK(ADDRESS, MASK)        ((ADDRESS) &= ~(MASK))
-#define CLEAR_BIT(ADDRESS, POS)             ((ADDRESS) &= ~(0x01UL << (POS)))
-//#define CLEAR_2BIT(ADDRESS, POS)            ((ADDRESS) &= ~(0x03UL << (POS)))
-//#define CLEAR_3BIT(ADDRESS, POS)            ((ADDRESS) &= ~(0x07UL << (POS)))
-//#define CLEAR_4BIT(ADDRESS, POS)            ((ADDRESS) &= ~(0x0FUL << (POS)))
-//#define CLEAR_5BIT(ADDRESS, POS)            ((ADDRESS) &= ~(0x1FUL << (POS)))
-//#define CLEAR_6BIT(ADDRESS, POS)            ((ADDRESS) &= ~(0x3FUL << (POS)))
-//#define CLEAR_7BIT(ADDRESS, POS)            ((ADDRESS) &= ~(0x7FUL << (POS)))
-//#define CLEAR_8BIT(ADDRESS, POS)            ((ADDRESS) &= ~(0xFFUL << (POS)))
-
-#define TOGGLE_BITMASK(ADDRESS, MASK)       ((ADDRESS) ^= (MASK))
-#define TOGGLE_BIT(ADDRESS, POS)            ((ADDRESS) ^= (0x01UL << (POS)))
-//#define TOGGLE_2BIT(ADDRESS, POS)           ((ADDRESS) ^= (0x03UL << (POS)))
-//#define TOGGLE_3BIT(ADDRESS, POS)           ((ADDRESS) ^= (0x07UL << (POS)))
-//#define TOGGLE_4BIT(ADDRESS, POS)           ((ADDRESS) ^= (0x0FUL << (POS)))
-//#define TOGGLE_5BIT(ADDRESS, POS)           ((ADDRESS) ^= (0x1FUL << (POS)))
-//#define TOGGLE_6BIT(ADDRESS, POS)           ((ADDRESS) ^= (0x3FUL << (POS)))
-//#define TOGGLE_7BIT(ADDRESS, POS)           ((ADDRESS) ^= (0x7FUL << (POS)))
-//#define TOGGLE_8BIT(ADDRESS, POS)           ((ADDRESS) ^= (0xFFUL << (POS)))
-
-#define WRITE_BIT( ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0x01UL << (POS))) | ((0x01UL & (VALUE)) << (POS)))
-#define WRITE_2BIT(ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0x03UL << (POS))) | ((0x03UL & (VALUE)) << (POS)))
-#define WRITE_3BIT(ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0x07UL << (POS))) | ((0x07UL & (VALUE)) << (POS)))
-#define WRITE_4BIT(ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0x0FUL << (POS))) | ((0x0FUL & (VALUE)) << (POS)))
-#define WRITE_5BIT(ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0x1FUL << (POS))) | ((0x1FUL & (VALUE)) << (POS)))
-#define WRITE_6BIT(ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0x3FUL << (POS))) | ((0x3FUL & (VALUE)) << (POS)))
-#define WRITE_7BIT(ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0x7FUL << (POS))) | ((0x7FUL & (VALUE)) << (POS)))
-#define WRITE_8BIT(ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0xFFUL << (POS))) | ((0xFFUL & (VALUE)) << (POS)))
+#define BM_TOGGLE_BITMASK(ADDRESS, MASK)       ((ADDRESS) ^= (MASK))
+#define BM_TOGGLE_BIT(ADDRESS, POS)            ((ADDRESS) ^= (0x01UL << (POS)))
 
 #if defined(__GNUC__) || defined(__clang__)
-#define GET_FIELD(ADDRESS, MASK)(          ((ADDRESS) & (MASK)) >> (__builtin_ctz(MASK)))
+/* MASK must not be 0 */
+#define BM_WRITE_FIELD(ADDRESS, MASK, VALUE) \
+    ((ADDRESS) = (((ADDRESS) & ~(MASK)) | (((VALUE) << (__builtin_ctz(MASK))) & (MASK))))
 #endif
 
-#define GET_BIT( ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0x01UL)
-#define GET_2BIT(ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0x03UL)
-#define GET_3BIT(ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0x07UL)
-#define GET_4BIT(ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0x0FUL)
-#define GET_5BIT(ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0x1FUL)
-#define GET_6BIT(ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0x3FUL)
-#define GET_7BIT(ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0x7FUL)
-#define GET_8BIT(ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0xFFUL)
+#define BM_WRITE_BIT( ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0x01UL << (POS))) | ((0x01UL & (VALUE)) << (POS)))
+#define BM_WRITE_2BIT(ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0x03UL << (POS))) | ((0x03UL & (VALUE)) << (POS)))
+#define BM_WRITE_3BIT(ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0x07UL << (POS))) | ((0x07UL & (VALUE)) << (POS)))
+#define BM_WRITE_4BIT(ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0x0FUL << (POS))) | ((0x0FUL & (VALUE)) << (POS)))
+#define BM_WRITE_5BIT(ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0x1FUL << (POS))) | ((0x1FUL & (VALUE)) << (POS)))
+#define BM_WRITE_6BIT(ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0x3FUL << (POS))) | ((0x3FUL & (VALUE)) << (POS)))
+#define BM_WRITE_7BIT(ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0x7FUL << (POS))) | ((0x7FUL & (VALUE)) << (POS)))
+#define BM_WRITE_8BIT(ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0xFFUL << (POS))) | ((0xFFUL & (VALUE)) << (POS)))
+
+#if defined(__GNUC__) || defined(__clang__)
+/* mask must not be 0 */
+#define BM_GET_FIELD(ADDRESS, MASK)(        	 ((ADDRESS) & (MASK)) >> (__builtin_ctz(MASK)))
+#endif
+
+#define BM_GET_BIT( ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0x01UL)
+#define BM_GET_2BIT(ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0x03UL)
+#define BM_GET_3BIT(ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0x07UL)
+#define BM_GET_4BIT(ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0x0FUL)
+#define BM_GET_5BIT(ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0x1FUL)
+#define BM_GET_6BIT(ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0x3FUL)
+#define BM_GET_7BIT(ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0x7FUL)
+#define BM_GET_8BIT(ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0xFFUL)
 
 #ifdef __cplusplus
 }
