44c4927d6d17e5c6e336d3f2d0c3072c46d38133
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-05-20 00:28:41 +0330

    Create qei_port.h

---
+++ platforms/avr8/ports/qei_port.h
@@ -0,0 +1,17 @@
+#ifndef QEI_PORT_INCLUDED
+#define QEI_PORT_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include "compiler_port.h"
+#include "qei_hw.h"
+#include "qei_types.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
