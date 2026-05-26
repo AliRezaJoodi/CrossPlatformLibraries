afd2bfd340fcbfdbe26c9a2ea1b3743b112a2998
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-05-26 07:54:40 +0330

    Update compiler_port.h

--- platforms/avr8/ports/compiler_port.h
+++ platforms/avr8/ports/compiler_port.h
@@ -11,25 +11,25 @@
     #include <avr/io.h>
     #include <util/delay.h>
 
-    #define DELAY_MS(ms)            _delay_ms(ms)
-    #define DELAY_US(us)            _delay_us(us)
-    #define NOP()                   __asm__ __volatile__("nop")
-    #define INTERRUPTS_SAVE()       uint8_t sreg_save = SREG;
-    #define INTERRUPTS_DISABLE()    cli()
-    #define INTERRUPTS_ENABLE()     sei()
-    #define INTERRUPTS_RESTORE()    SREG = sreg_save
+    #define DELAY_MS(ms)                    _delay_ms(ms)
+    #define DELAY_US(us)                    _delay_us(us)
+    #define NOP()                           __asm__ __volatile__("nop")
+    #define INT_GLOBAL_SAVE(sreg_save)      sreg_save = SREG;
+    #define INT_GLOBAL_DISABLE()            cli()
+    #define INT_GLOBAL_ENABLE()             sei()
+    #define INT_GLOBAL_RESTORE(sreg_save)   SREG = sreg_save
 
 #elif defined(__CODEVISIONAVR__)
     #include <io.h>
     #include <delay.h>
 
-    #define DELAY_MS(ms)            delay_ms(ms)
-    #define DELAY_US(us)            delay_us(us)
-    #define NOP()                   #asm("nop")
-    #define INTERRUPTS_SAVE()       uint8_t sreg_save = SREG;
-    #define INTERRUPTS_DISABLE()    #asm("cli")
-    #define INTERRUPTS_ENABLE()     #asm("sei")
-    #define INTERRUPTS_RESTORE()    SREG = sreg_save
+    #define DELAY_MS(ms)                    delay_ms(ms)
+    #define DELAY_US(us)                    delay_us(us)
+    #define NOP()                           #asm("nop")
+    #define INT_GLOBAL_SAVE(sreg_save)      sreg_save = SREG;
+    #define INT_GLOBAL_DISABLE()            #asm("cli")
+    #define INT_GLOBAL_ENABLE()             #asm("sei")
+    #define INT_GLOBAL_RESTORE(sreg_save)   SREG = sreg_save
 
 #elif defined(__ICCAVR__)
     #ifndef F_CPU
@@ -38,13 +38,13 @@
 
     #include <ioavr.h>
 
-    #define DELAY_MS(ms)            __delay_cycles((F_CPU/1000UL)*(ms))
-    #define DELAY_US(us)            __delay_cycles((F_CPU/1000000UL)*(us))
-    #define NOP()                   __no_operation()  // or asm("nop")
-    #define INTERRUPTS_SAVE()       __istate_t sreg_save = __get_interrupt_state();
-    #define INTERRUPTS_DISABLE()    __disable_interrupt()
-    #define INTERRUPTS_ENABLE()     __enable_interrupt()
-    #define INTERRUPTS_RESTORE()    __set_interrupt_state(sreg_save)
+    #define DELAY_MS(ms)                    __delay_cycles((F_CPU/1000UL)*(ms))
+    #define DELAY_US(us)                    __delay_cycles((F_CPU/1000000UL)*(us))
+    #define NOP()                           __no_operation()  // or asm("nop")
+    #define INT_GLOBAL_SAVE(sreg_save)      (sreg_save) = __get_interrupt_state()
+    #define INT_GLOBAL_DISABLE()            __disable_interrupt()
+    #define INT_GLOBAL_ENABLE()             __enable_interrupt()
+    #define INT_GLOBAL_RESTORE(sreg_save)   __set_interrupt_state(sreg_save)
 
 #else
     #error "Unsupported Compiler"
