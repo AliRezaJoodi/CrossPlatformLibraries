798582ef11231a27fe6d3ad629218f5c32f2280b
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-05-27 16:44:21 +0330

    chore: update, avr8/mcp3208

--- platforms/avr8/ports/mcp3208_hw.h
+++ platforms/avr8/ports/mcp3208_hw.h
@@ -1,6 +1,15 @@
 #ifndef MCP3208_HW_INCLUDED
 #define MCP3208_HW_INCLUDED
 
+#include "hardware.h"       /**< Project-level overrides */
+
+#ifndef MCP3208_HARDWARE
+#define MCP3208_HARDWARE
+    #define MCP3208_TIMEOUT         1000U
+
+    #warning "MCP3208_HARDWARE is not defined; default configuration will be used."
+#endif
+
 #ifndef MCP3208_HARDWARE_EXTRA
 #define MCP3208_HARDWARE_EXTRA
     #define MCP3208_CS_DDR          DDRB
