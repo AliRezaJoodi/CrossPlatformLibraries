dd680c7a30d9b4f0b2482263b06ecc9b23239fa8
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-05-27 16:19:36 +0330

    chore: initial setup, stm32f4/buzzer

---
+++ platforms/stm32f4/ports/buzzer_port.h
@@ -0,0 +1,30 @@
+#ifndef BUZZER_PORT_INCLUDED
+#define BUZZER_PORT_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include "gpio.h"
+#include "buzzer_hw.h"
+
+static inline void Buzzer_Pin_ConfigAsOutput(void){
+	GPIO_ConfigDirection(BUZZER_GPIO, BUZZER_PIN, GPIO_MODE_OUTPUT);
+	GPIO_ConfigOutputType(BUZZER_GPIO, BUZZER_PIN, GPIO_OUTPUT_PUSHPULL);
+	GPIO_ConfigOutputSpeed(BUZZER_GPIO, BUZZER_PIN, GPIO_SPEED_2MHZ);
+}
+
+static inline void Buzzer_Pin_Set(void){
+	GPIO_SetPinMask(BUZZER_GPIO, BUZZER_MASK);
+}
+
+static inline void Buzzer_Pin_Clear(void){
+	GPIO_ClearPinMask(BUZZER_GPIO, BUZZER_MASK);
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* BUZZER_PORT_INCLUDED */
