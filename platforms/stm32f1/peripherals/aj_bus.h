a86bd7f9ae3ea05f583188f79fa8b1ab76bd57aa
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-06-01 06:41:23 +0330

    chore: update, stm32f1/aj_bus

--- platforms/stm32f1/peripherals/aj_bus.h
+++ platforms/stm32f1/peripherals/aj_bus.h
@@ -26,113 +26,155 @@ extern "C" {
 #include "aj_bus_type.h"
 
 /**
- * @brief  Enable or disable the clock of an AHB peripheral.
- * @param  Periph	AHB peripheral bit position in RCC->AHBENR.
- * @param  mode  	Clock enable state.
+ * @brief  Enable the clock of the specified AHB peripheral(s).
+ * @param  periphs  Bit mask of AHB peripherals in RCC->AHBENR.
  * @note   A read-back is performed after enabling to ensure the write is applied
  *         before further peripheral access.
  */
-static inline void AJ_BUS_AHB_ConfigClockEnableStatus(AJ_BUS_AHB_Periph_t Periph, AJ_BUS_ClockEnableStatus_t mode){
-	WriteBit_Reg32(&(RCC->AHBENR), Periph, mode);
-	
-	if(mode == AJ_BUS_ENABLE){
-		volatile uint32_t tmpreg = READ_BIT(RCC->AHBENR, (1UL << (uint32_t)Periph));
-		(void)tmpreg;
-	}
+static inline void AJ_BUS_AHB_EnableClock(AJ_BUS_AHB_PeriphMask_t periphs){
+	volatile uint32_t tmpreg;
+
+	SetBitMask_Reg32(&(RCC->AHBENR), periphs);
+
+	tmpreg = READ_BIT(RCC->AHBENR, periphs);
+	(void)tmpreg;
 }
 
 /**
- * @brief  Get AHB peripheral clock enable state.
- * @param  Periph	AHB peripheral bit position in RCC->AHBENR.
- * @retval Clock 	enable state.
+ * @brief  Disable the clock of the specified AHB peripheral(s).
+ * @param  periphs  Bit mask of AHB peripherals in RCC->AHBENR.
  */
-static inline AJ_BUS_ClockEnableStatus_t AJ_BUS_AHB_GetClockEnableStatus(AJ_BUS_AHB_Periph_t Periph){
-	return GetBit_Reg32(&(RCC->AHBENR), Periph);
+static inline void AJ_BUS_AHB_DisableClock(AJ_BUS_AHB_PeriphMask_t periphs){
+	ClearBitMask_Reg32(&(RCC->AHBENR), periphs);
+}
+
+/**
+ * @brief  Check whether the specified AHB peripheral clock(s) are enabled.
+ * @param  periphs  Bit mask of AHB peripherals in RCC->AHBENR.
+ * @retval Clock enable status.
+ */
+static inline AJ_BUS_ClockEnableStatus_t AJ_BUS_AHB_IsClockEnabled(AJ_BUS_AHB_PeriphMask_t periphs){
+	return IsBitMaskSet_Reg32(&(RCC->AHBENR), periphs);
 }
 
 #if defined(RCC_AHBRSTR_SUPPORT)
 /**
- * @brief  Force or release reset of an AHB peripheral.
- * @param  Periph AHB peripheral bit position in RCC->AHBRSTR.
- * @param  mode   Reset state.
- * @note   After a reset force/release sequence, the peripheral registers return
+ * @brief  Force reset of the specified AHB peripheral(s).
+ * @param  periphs  Bit mask of AHB peripherals in RCC->AHBRSTR.
+ */
+static inline void AJ_BUS_AHB_ForceReset(AJ_BUS_AHB_PeriphMask_t periphs){
+	SetBitMask_Reg32(&(RCC->AHBRSTR), periphs);
+}
+
+/**
+ * @brief  Release reset of the specified AHB peripheral(s).
+ * @param  periphs  Bit mask of AHB peripherals in RCC->AHBRSTR.
+ * @note   After a force/release reset sequence, the peripheral registers return
  *         to their reset values. The peripheral must be reconfigured before reuse.
  */
-static inline void AJ_BUS_AHB_ConfigResetStatus(AJ_BUS_AHB_Periph_t Periph, AJ_BUS_ResetStatus_t mode){
-	WriteBit_Reg32(&(RCC->AHBRSTR), Periph, mode);
+static inline void AJ_BUS_AHB_ReleaseReset(AJ_BUS_AHB_PeriphMask_t periphs){
+	ClearBitMask_Reg32(&(RCC->AHBRSTR), periphs);
 }
 #endif /* RCC_AHBRSTR_SUPPORT */
 
 /**
- * @brief  Enable or disable the clock of an APB1 peripheral.
- * @param  Periph	APB1 peripheral bit position in RCC->APB1ENR.
- * @param  mode  	Clock enable state.
+ * @brief  Enable the clock of the specified APB1 peripheral(s).
+ * @param  periphs  Bit mask of APB1 peripherals in RCC->APB1ENR.
  * @note   A read-back is performed after enabling to ensure the write is applied
  *         before further peripheral access.
  */
-static inline void AJ_BUS_APB1_ConfigClockEnableStatus(AJ_BUS_APB1_Periph_t Periph, AJ_BUS_ClockEnableStatus_t mode){
-	WriteBit_Reg32(&(RCC->APB1ENR), Periph, mode);
-	
-	if(mode == AJ_BUS_ENABLE){
-		volatile uint32_t tmpreg = READ_BIT(RCC->APB1ENR, (1UL << (uint32_t)Periph));
-		(void)tmpreg;
-	}
+static inline void AJ_BUS_APB1_EnableClock(AJ_BUS_APB1_PeriphMask_t periphs){
+	volatile uint32_t tmpreg;
+
+	SetBitMask_Reg32(&(RCC->APB1ENR), periphs);
+
+	tmpreg = READ_BIT(RCC->APB1ENR, periphs);
+	(void)tmpreg;
 }
 
 /**
- * @brief  Get APB1 peripheral clock enable state.
- * @param  Periph	APB1 peripheral bit position in RCC->APB1ENR.
- * @retval Clock 	enable state.
+ * @brief  Disable the clock of the specified APB1 peripheral(s).
+ * @param  periphs  Bit mask of APB1 peripherals in RCC->APB1ENR.
  */
-static inline AJ_BUS_ClockEnableStatus_t AJ_BUS_APB1_GetClockEnableStatus(AJ_BUS_APB1_Periph_t Periph){
-	return GetBit_Reg32(&(RCC->APB1ENR), Periph);
+static inline void AJ_BUS_APB1_DisableClock(AJ_BUS_APB1_PeriphMask_t periphs){
+	ClearBitMask_Reg32(&(RCC->APB1ENR), periphs);
 }
 
 /**
- * @brief  Force or release reset of an APB1 peripheral.
- * @param  Periph APB1 peripheral bit position in RCC->APB1RSTR.
- * @param  mode   Reset state.
- * @note   After a reset force/release sequence, the peripheral registers return
+ * @brief  Check whether the specified APB1 peripheral clock(s) are enabled.
+ * @param  periphs  Bit mask of APB1 peripherals in RCC->APB1ENR.
+ * @retval Clock enable status.
+ */
+static inline AJ_BUS_ClockEnableStatus_t AJ_BUS_APB1_IsClockEnabled(AJ_BUS_APB1_PeriphMask_t periphs){
+	return IsBitMaskSet_Reg32(&(RCC->APB1ENR), periphs);
+}
+
+/**
+ * @brief  Force reset of the specified APB1 peripheral(s).
+ * @param  periphs  Bit mask of APB1 peripherals in RCC->APB1RSTR.
+ */
+static inline void AJ_BUS_APB1_ForceReset(AJ_BUS_APB1_PeriphMask_t periphs){
+	SetBitMask_Reg32(&(RCC->APB1RSTR), periphs);
+}
+
+/**
+ * @brief  Release reset of the specified APB1 peripheral(s).
+ * @param  periphs  Bit mask of APB1 peripherals in RCC->APB1RSTR.
+ * @note   After a force/release reset sequence, the peripheral registers return
  *         to their reset values. The peripheral must be reconfigured before reuse.
  */
-static inline void AJ_BUS_APB1_ConfigResetStatus(AJ_BUS_APB1_Periph_t Periph, AJ_BUS_ResetStatus_t mode){
-	WriteBit_Reg32(&(RCC->APB1RSTR), Periph, mode);
+static inline void AJ_BUS_APB1_ReleaseReset(AJ_BUS_APB1_PeriphMask_t periphs){
+	ClearBitMask_Reg32(&(RCC->APB1RSTR), periphs);
 }
 
 /**
- * @brief  Enable or disable the clock of an APB2 peripheral.
- * @param  Periph	APB2 peripheral bit position in RCC->APB2ENR.
- * @param  mode  	Clock enable state.
+ * @brief  Enable the clock of the specified APB2 peripheral(s).
+ * @param  periphs  Bit mask of APB2 peripherals in RCC->APB2ENR.
  * @note   A read-back is performed after enabling to ensure the write is applied
  *         before further peripheral access.
  */
-static inline void AJ_BUS_APB2_ConfigClockEnableStatus(AJ_BUS_APB2_Periph_t Periph, AJ_BUS_ClockEnableStatus_t mode){
-	WriteBit_Reg32(&(RCC->APB2ENR), Periph, mode);
+static inline void AJ_BUS_APB2_EnableClock(AJ_BUS_APB2_PeriphMask_t periphs){
+	volatile uint32_t tmpreg;
+
+	SetBitMask_Reg32(&(RCC->APB2ENR), periphs);
 	
-	if(mode == AJ_BUS_ENABLE){
-		volatile uint32_t tmpreg = READ_BIT(RCC->APB2ENR, (1UL << (uint32_t)Periph));
-		(void)tmpreg;
-	}
+	tmpreg = READ_BIT(RCC->APB2ENR, periphs);
+	(void)tmpreg;
 }
 
 /**
- * @brief  Get APB2 peripheral clock enable state.
- * @param  Periph	APB2 peripheral bit position in RCC->APB2ENR.
- * @retval Clock 	enable state.
+ * @brief  Disable the clock of the specified APB2 peripheral(s).
+ * @param  periphs  Bit mask of APB2 peripherals in RCC->APB2ENR.
  */
-static inline AJ_BUS_ClockEnableStatus_t  AJ_BUS_APB2_GetClockEnableStatus(AJ_BUS_APB2_Periph_t Periph){
-	return GetBit_Reg32(&(RCC->APB2ENR), Periph);
+static inline void AJ_BUS_APB2_DisableClock(AJ_BUS_APB2_PeriphMask_t periphs){
+	ClearBitMask_Reg32(&(RCC->APB2ENR), periphs);
 }
 
 /**
- * @brief  Force or release reset of an APB2 peripheral.
- * @param  Periph APB2 peripheral bit position in RCC->APB2RSTR.
- * @param  mode   Reset state.
- * @note   After a reset force/release sequence, the peripheral registers return
+ * @brief  Check whether the specified APB2 peripheral clock(s) are enabled.
+ * @param  periphs  Bit mask of APB2 peripherals in RCC->APB2ENR.
+ * @retval Clock enable status.
+ */
+static inline AJ_BUS_ClockEnableStatus_t  AJ_BUS_APB2_IsClockEnabled(AJ_BUS_APB2_PeriphMask_t periphs){
+	return IsBitMaskSet_Reg32(&(RCC->APB2ENR), periphs);
+}
+
+/**
+ * @brief  Force reset of the specified APB2 peripheral(s).
+ * @param  periphs  Bit mask of APB2 peripherals in RCC->APB2RSTR.
+ */
+static inline void AJ_BUS_APB2_ForceReset(AJ_BUS_APB2_PeriphMask_t periphs){
+	SetBitMask_Reg32(&(RCC->APB2RSTR), periphs);
+}
+
+/**
+ * @brief  Release reset of the specified APB2 peripheral(s).
+ * @param  periphs  Bit mask of APB2 peripherals in RCC->APB2RSTR.
+ * @note   After a force/release reset sequence, the peripheral registers return
  *         to their reset values. The peripheral must be reconfigured before reuse.
  */
-static inline void AJ_BUS_APB2_ConfigResetStatus(AJ_BUS_APB2_Periph_t Periph, AJ_BUS_ResetStatus_t mode){
-	WriteBit_Reg32(&(RCC->APB2RSTR), Periph, mode);
+static inline void AJ_BUS_APB2_ReleaseReset(AJ_BUS_APB2_PeriphMask_t periphs){
+	ClearBitMask_Reg32(&(RCC->APB2RSTR), periphs);
 }
 
 #ifdef __cplusplus
