8a4bf0f5f48168a9455df556044786f07f99751d
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-06-10 02:15:30 +0330

    refactor: reorganize, aj_bit

---
+++ utils/aj_bit.h
@@ -0,0 +1,21 @@
+// GitHub Account: GitHub.com/AliRezaJoodi
+
+#ifndef AJ_BIT_INCLUDED
+#define AJ_BIT_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+#include "aj_bit_macro.h"
+#include "aj_bit_u8.h"
+#include "aj_bit_u16.h"
+#include "aj_bit_u32.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
