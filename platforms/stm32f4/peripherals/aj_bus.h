906cf9893bb986f91e603eb9dfeda6bb654a1769
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-06-25 01:07:37 +0330

    Update aj_bus.h

--- platforms/stm32f4/peripherals/aj_bus.h
+++ platforms/stm32f4/peripherals/aj_bus.h
@@ -10,26 +10,26 @@ extern "C" {
 #include "aj_bit_reg.h"
 #include "aj_bus_type.h"
 
-static inline void AJ_BUS_AHB1_EnableClock(aj_bus_ahb1_mask_t periphs){
+static inline void AJ_BUS_AHB1_EnableClock(aj_bus_ahb1_clock_mask_t periph){
 }
 
-static inline void AJ_BUS_AHB1_DisableClock(aj_bus_ahb1_mask_t periphs){
+static inline void AJ_BUS_AHB1_DisableClock(aj_bus_ahb1_clock_mask_t periph){
 }
 
-static inline aj_bus_clock_state_t AJ_BUS_AHB1_IsClockEnabled(aj_bus_ahb1_mask_t periphs){
+static inline aj_bus_clock_state_t AJ_BUS_AHB1_IsClockEnabled(aj_bus_ahb1_clock_mask_t periph){
 	return 0;
 }
 
-static inline void AJ_BUS_AHB1_ForceReset(aj_bus_ahb1_mask_t periphs){	
+static inline void AJ_BUS_AHB1_ForceReset(aj_bus_ahb1_reset_mask_t periph){	
 }
 
-static inline void AJ_BUS_AHB1_ReleaseReset(aj_bus_ahb1_mask_t periphs){
+static inline void AJ_BUS_AHB1_ReleaseReset(aj_bus_ahb1_reset_mask_t periph){
 }
 
-static inline void AJ_BUS_AHB1_EnableClockInLowPower(aj_bus_ahb1_mask_t periphs){
+static inline void AJ_BUS_AHB1_EnableClockInLowPower(aj_bus_ahb1_clock_lp_mask_t periph){
 }
 
-static inline void AJ_BUS_AHB1_DisableClockInLowPower(aj_bus_ahb1_mask_t periphs){
+static inline void AJ_BUS_AHB1_DisableClockInLowPower(aj_bus_ahb1_clock_lp_mask_t periph){
 }
 
 #ifdef __cplusplus
