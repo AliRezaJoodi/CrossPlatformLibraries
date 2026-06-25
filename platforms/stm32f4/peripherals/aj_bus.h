fb0f632780b0038219ebcd9b093dba97c3d93a06
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-06-25 17:56:36 +0330

    chore: update, stm32f4/aj_bus

--- platforms/stm32f4/peripherals/aj_bus.h
+++ platforms/stm32f4/peripherals/aj_bus.h
@@ -15,27 +15,41 @@ extern "C" {
 #include "aj_bit_reg.h"
 #include "aj_bus_type.h"
 
-static inline void AJ_BUS_AHB1_EnableClock(aj_bus_ahb1_clock_mask_t periph){
+//****************************************************************************
+static inline void AJ_BUS_AHB1_EnableClock(aj_bus_ahb1_clock_mask_t periphs){
+	AJ_BitReg_SetBit_Mask(&(RCC->AHB1ENR), periphs);
+
+	volatile uint32_t dummy = RCC->AHB1ENR;
+	(void)dummy;
 }
 
-static inline void AJ_BUS_AHB1_DisableClock(aj_bus_ahb1_clock_mask_t periph){
+static inline aj_bus_clock_state_t AJ_BUS_AHB1_IsClockEnabled(aj_bus_ahb1_clock_mask_t periphs){
+	return AJ_BitReg_IsBitSet_Mask(&(RCC->AHB1ENR), periphs);
 }
 
-static inline aj_bus_clock_state_t AJ_BUS_AHB1_IsClockEnabled(aj_bus_ahb1_clock_mask_t periph){
-	return 0;
+static inline void AJ_BUS_AHB1_DisableClock(aj_bus_ahb1_clock_mask_t periphs){
+	AJ_BitReg_ClearBit_Mask(&(RCC->AHB1ENR), periphs);
 }
 
-static inline void AJ_BUS_AHB1_ForceReset(aj_bus_ahb1_reset_mask_t periph){	
+static inline void AJ_BUS_AHB1_ForceReset(aj_bus_ahb1_reset_mask_t periphs){
+	AJ_BitReg_SetBit_Mask(&(RCC->AHB1RSTR), periphs);
 }
 
-static inline void AJ_BUS_AHB1_ReleaseReset(aj_bus_ahb1_reset_mask_t periph){
+static inline void AJ_BUS_AHB1_ReleaseReset(aj_bus_ahb1_reset_mask_t periphs){
+	AJ_BitReg_ClearBit_Mask(&(RCC->AHB1RSTR), periphs);
 }
 
-static inline void AJ_BUS_AHB1_EnableClockInLowPower(aj_bus_ahb1_clock_lp_mask_t periph){
+static inline void AJ_BUS_AHB1_EnableClockInLowPower(aj_bus_ahb1_clock_lp_mask_t periphs){
+	AJ_BitReg_SetBit_Mask(&(RCC->AHB1LPENR), periphs);
+
+	volatile uint32_t dummy = RCC->AHB1LPENR;
+	(void)dummy;
 }
 
 static inline void AJ_BUS_AHB1_DisableClockInLowPower(aj_bus_ahb1_clock_lp_mask_t periphs){
+	AJ_BitReg_ClearBit_Mask(&(RCC->AHB1LPENR), periphs);
 }
+
 
 #ifdef __cplusplus
 }
