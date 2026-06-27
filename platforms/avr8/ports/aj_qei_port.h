b6c0401efaac11d09acdcc9d3f33e15cf5b7c0e7
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-06-28 00:49:57 +0330

    Update aj_qei_port.h

--- platforms/avr8/ports/aj_qei_port.h
+++ platforms/avr8/ports/aj_qei_port.h
@@ -6,45 +6,45 @@ extern "C" {
 #endif
 
 #include <stdint.h>
-#include "aj_compiler_port.h"
+#include "aj_compiler.h"
 #include "aj_bit_reg.h"
 #include "aj_qei_hw.h"
 #include "aj_qei_type.h"
 
 //*************************************************
 static inline void AJ_QEI_A_ConfigAsInput(const aj_qei_t *qei){
-    AJ_BitReg_ClearBits_Mask(qei->chA.ddr, qei->chA.mask);
+    AJ_BitReg_ClearBit_Mask(qei->chA.ddr, qei->chA.mask);
 }
 
 static inline void AJ_QEI_A_ConfigAsPullNone(const aj_qei_t *qei){
-    AJ_BitReg_ClearBits_Mask(qei->chA.port, qei->chA.mask);
+    AJ_BitReg_ClearBit_Mask(qei->chA.port, qei->chA.mask);
 }
 
 static inline void AJ_QEI_A_ConfigAsPullUp(const aj_qei_t *qei){
-    AJ_BitReg_SetBits_Mask(qei->chA.port, qei->chA.mask);
+    AJ_BitReg_SetBit_Mask(qei->chA.port, qei->chA.mask);
 }
 
 //*************************************************
 static inline void AJ_QEI_B_ConfigAsInput(const aj_qei_t *qei){
-    AJ_BitReg_ClearBits_Mask(qei->chB.ddr, qei->chB.mask);
+    AJ_BitReg_ClearBit_Mask(qei->chB.ddr, qei->chB.mask);
 }
 
 static inline void AJ_QEI_B_ConfigAsPullNone(const aj_qei_t *qei){
-    AJ_BitReg_ClearBits_Mask(qei->chB.port, qei->chB.mask);
+    AJ_BitReg_ClearBit_Mask(qei->chB.port, qei->chB.mask);
 }
 
 static inline void AJ_QEI_B_ConfigAsPullUp(const aj_qei_t *qei){
-    AJ_BitReg_SetBits_Mask(qei->chB.port, qei->chB.mask);
+    AJ_BitReg_SetBit_Mask(qei->chB.port, qei->chB.mask);
 }
 
 //*************************************************
 #if (AJ_QEI_AB_CLOSED == 0U)
     static inline uint8_t AJ_QEI_A_Read(const aj_qei_t *qei){
-        return AJ_BitReg_AreBitsSet_Mask(qei->chA.pin, qei->chA.mask);
+        return AJ_BitReg_IsBitSet_Mask(qei->chA.pin, qei->chA.mask);
     }
 
     static inline uint8_t AJ_QEI_B_Read(const aj_qei_t *qei){
-        return AJ_BitReg_AreBitsSet_Mask(qei->chB.pin, qei->chB.mask);
+        return AJ_BitReg_IsBitSet_Mask(qei->chB.pin, qei->chB.mask);
     }
 #else
     static inline uint8_t AJ_QEI_AB_Read(const aj_qei_t *qei){
