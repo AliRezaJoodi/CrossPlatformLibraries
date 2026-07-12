394bc17b50fc5ae7f91be55d09714d6b8a947590
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-07-12 22:23:51 +0330

    Update aj_keypad4x4.h

--- drivers/aj_keypad4x4.h
+++ drivers/aj_keypad4x4.h
@@ -25,6 +25,10 @@
  * - `aj_target.h`
  * - `aj_keypad4x4_config_platform.h`
  *
+ * To customize these configurations, override them inside the central project
+ * hardware configuration file:
+ * - `hardware.h`
+ *
  * @author  AliReza Joodi
  * @see     https://github.com/AliRezaJoodi
  */
