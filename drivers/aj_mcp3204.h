7306512b352558c634570a9d6f61067e1f655c42
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-07-16 19:50:10 +0330

    Create aj_mcp3204.h

---
+++ drivers/aj_mcp3204.h
@@ -0,0 +1,64 @@
+/**
+ * @brief   MCP3204 model-specific wrapper for `aj_mcp3204_3208.h`.
+ *
+ * @details
+ * For driver requirements, usage notes, and the generic API description,
+ * refer to `aj_mcp3204_3208.h`.
+ *
+ * If only `MCP3204` is used from this device family, the configuration macros
+ * from `aj_mcp3204_3208_config.h` should be overridden as follows:
+ * - `AJ_MCP3204_3208_CHANNELS` : `4U`
+ *
+ * @author  AliReza Joodi
+ * @see     https://github.com/AliRezaJoodi
+ */
+
+#ifndef AJ_MCP3204_INCLUDED
+#define AJ_MCP3204_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#include <stdint.h>
+#include "aj_mcp3204_3208.h"
+#include "aj_mcp3204_config_platform.h"        /**< refer to main.c*/
+
+/* Valid symbolic values for aj_mcp3204_3208_channel_t. */
+#define AJ_MCP3204_CH0                  AJ_MCP3204_3208_CH0
+#define AJ_MCP3204_CH1                  AJ_MCP3204_3208_CH1
+#define AJ_MCP3204_CH2                  AJ_MCP3204_3208_CH2
+#define AJ_MCP3204_CH3                  AJ_MCP3204_3208_CH3
+#define AJ_MCP3204_CH4                  AJ_MCP3204_3208_CH4
+#define AJ_MCP3204_CH5                  AJ_MCP3204_3208_CH5
+#define AJ_MCP3204_CH6                  AJ_MCP3204_3208_CH6
+#define AJ_MCP3204_CH7                  AJ_MCP3204_3208_CH7
+#define AJ_MCP3204_CH0CH1               AJ_MCP3204_3208_CH0CH1
+#define AJ_MCP3204_CH1CH0               AJ_MCP3204_3208_CH1CH0
+#define AJ_MCP3204_CH2CH3               AJ_MCP3204_3208_CH2CH3
+#define AJ_MCP3204_CH3CH2               AJ_MCP3204_3208_CH3CH2
+#define AJ_MCP3204_CH4CH5               AJ_MCP3204_3208_CH4CH5
+#define AJ_MCP3204_CH5CH4               AJ_MCP3204_3208_CH5CH4
+#define AJ_MCP3204_CH6CH7               AJ_MCP3204_3208_CH6CH7
+#define AJ_MCP3204_CH7CH6               AJ_MCP3204_3208_CH7CH6
+
+typedef aj_mcp3204_3208_t               aj_mcp3204_t;
+typedef aj_mcp3204_3208_channel_t       aj_mcp3204_channel_t;
+
+/** Initializes the MCP3204 ADC. */
+static inline void AJ_MCP3204_Init(aj_mcp3204_t *mcp){
+    AJ_MCP3204_3208_Init(mcp);
+}
+
+/** Reads and returns the raw 12-bit ADC value from the specified MCP3204 channel. */
+static inline uint16_t AJ_MCP3204_ReadRaw(aj_mcp3204_t *mcp, aj_mcp3204_channel_t ch){
+    return AJ_MCP3204_3208_ReadRaw(mcp, ch);
+}
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
