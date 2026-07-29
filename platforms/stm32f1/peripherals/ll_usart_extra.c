0d872161c8db706d56aeff7aa27cf902bc33a610
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-07-29 23:27:49 +0330

    chore: update, ll_usart_extra

--- platforms/stm32f1/peripherals/ll_usart_extra.c
+++ platforms/stm32f1/peripherals/ll_usart_extra.c
@@ -1,6 +1,45 @@
 // GitHub Account: GitHub.com/AliRezaJoodi
 
 #include "ll_usart_extra.h"
+#include <stddef.h>
+
+static const char *p_tx_data = NULL;
+static volatile uint8_t is_sending = 0;
+
+//********************************
+void LL_USART_TransmitString_IT(USART_TypeDef *USARTx, const char *str) {
+    if (is_sending || str == NULL || *str == '\0'){
+			return;
+		}
+
+    p_tx_data = str;
+    is_sending = 1;
+
+    LL_USART_EnableIT_TXE(USARTx);
+}
+
+//********************************
+uint8_t LL_USART_IsSending(USART_TypeDef *USARTx) {
+    return is_sending;
+}
+
+//********************************
+void LL_USART_Transmit_IRQHandler(USART_TypeDef *USARTx) {
+    if (LL_USART_IsEnabledIT_TXE(USARTx) && LL_USART_IsActiveFlag_TXE(USARTx)) {
+        if (*p_tx_data != '\0') {
+            LL_USART_TransmitData8(USARTx, *p_tx_data++);
+        } else {
+            LL_USART_DisableIT_TXE(USARTx);
+            LL_USART_EnableIT_TC(USARTx);
+        }
+    }
+
+    if (LL_USART_IsEnabledIT_TC(USARTx) && LL_USART_IsActiveFlag_TC(USARTx)) {
+        LL_USART_ClearFlag_TC(USARTx);
+        LL_USART_DisableIT_TC(USARTx);
+        is_sending = 0; 
+    }
+}
 
 //********************************
 void LL_USART_PutChar(USART_TypeDef *USARTx, char data){
@@ -10,7 +49,7 @@ void LL_USART_PutChar(USART_TypeDef *USARTx, char data){
 }
 
 //********************************
-void LL_USART_PutString(USART_TypeDef *USARTx, const char *str){
+void LL_USART_TransmitString(USART_TypeDef *USARTx, const char *str){
   while(*str != 0){
 		LL_USART_PutChar(USARTx,*str);
     str++;
--- platforms/stm32f1/peripherals/ll_usart_extra.h
+++ platforms/stm32f1/peripherals/ll_usart_extra.h
@@ -7,11 +7,17 @@
 extern "C" {
 #endif
 
+
 #include <stm32f1xx.h>
 #include <stm32f1xx_ll_usart.h>
 	
 void LL_USART_PutChar(USART_TypeDef *USARTx, char data);
-void LL_USART_PutString(USART_TypeDef *USARTx, const char *str);
+void LL_USART_TransmitString(USART_TypeDef *USARTx, const char *str);
+
+void LL_USART_TransmitString_IT(USART_TypeDef *USARTx, const char *str);
+uint8_t LL_USART_IsSending(USART_TypeDef *USARTx);
+void LL_USART_Transmit_IRQHandler(USART_TypeDef *USARTx);
+
 
 #ifdef __cplusplus
 }
