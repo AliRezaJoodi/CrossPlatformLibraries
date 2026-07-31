b4cfd2a9bca3f50b7c5dfb383740ba5da30d74ee
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-07-31 13:47:23 +0330

    chore: update, ll_usart_extra

--- platforms/stm32f1/peripherals/ll_usart_extra.c
+++ platforms/stm32f1/peripherals/ll_usart_extra.c
@@ -3,31 +3,31 @@
 #include "ll_usart_extra.h"
 #include <stddef.h>
 
-static const char *p_tx_data = NULL;
-static volatile uint8_t is_sending = 0;
+static const char *tx_data_p = NULL;
+static volatile uint8_t sending = 0;
 
 //********************************
 void LL_USART_TransmitString_IT(USART_TypeDef *USARTx, const char *str) {
-    if (is_sending || str == NULL || *str == '\0'){
+    if (sending == 1 || str == NULL || *str == '\0'){
 			return;
 		}
 
-    p_tx_data = str;
-    is_sending = 1;
+    tx_data_p = str;
+    sending = 1;
 
     LL_USART_EnableIT_TXE(USARTx);
 }
 
 //********************************
-uint8_t LL_USART_IsSending(USART_TypeDef *USARTx) {
-    return is_sending;
+uint8_t LL_USART_IsTransmitBusy(USART_TypeDef *USARTx) {
+    return sending;
 }
 
 //********************************
-void LL_USART_Transmit_IRQHandler(USART_TypeDef *USARTx) {
+void LL_USART_Transmit_Handler(USART_TypeDef *USARTx) {
     if (LL_USART_IsEnabledIT_TXE(USARTx) && LL_USART_IsActiveFlag_TXE(USARTx)) {
-        if (*p_tx_data != '\0') {
-            LL_USART_TransmitData8(USARTx, *p_tx_data++);
+        if (*tx_data_p != '\0') {
+            LL_USART_TransmitData8(USARTx, *tx_data_p++);
         } else {
             LL_USART_DisableIT_TXE(USARTx);
             LL_USART_EnableIT_TC(USARTx);
@@ -37,12 +37,12 @@ void LL_USART_Transmit_IRQHandler(USART_TypeDef *USARTx) {
     if (LL_USART_IsEnabledIT_TC(USARTx) && LL_USART_IsActiveFlag_TC(USARTx)) {
         LL_USART_ClearFlag_TC(USARTx);
         LL_USART_DisableIT_TC(USARTx);
-        is_sending = 0; 
+        sending = 0; 
     }
 }
 
 //********************************
-void LL_USART_PutChar(USART_TypeDef *USARTx, char data){
+void LL_USART_TransmitChar(USART_TypeDef *USARTx, char data){
 	while(LL_USART_IsActiveFlag_TXE(USARTx) != 1){}		// Transmit Data Register Empty 
 	LL_USART_TransmitData8(USARTx,data);
 //	while(LL_USART_IsActiveFlag_TC(USARTx) != 1){}		// Transmission Complete 
@@ -51,9 +51,9 @@ void LL_USART_PutChar(USART_TypeDef *USARTx, char data){
 //********************************
 void LL_USART_TransmitString(USART_TypeDef *USARTx, const char *str){
   while(*str != 0){
-		LL_USART_PutChar(USARTx,*str);
+		LL_USART_TransmitChar(USARTx,*str);
     str++;
   }
-	LL_USART_PutChar(USARTx, '\r');		// 0x0D
-	LL_USART_PutChar(USARTx, '\n');		// 0x0A
+//	LL_USART_TransmitChar(USARTx, '\r');		// 0x0D
+//	LL_USART_TransmitChar(USARTx, '\n');		// 0x0A
 }
--- platforms/stm32f1/peripherals/ll_usart_extra.h
+++ platforms/stm32f1/peripherals/ll_usart_extra.h
@@ -11,12 +11,12 @@ extern "C" {
 #include <stm32f1xx.h>
 #include <stm32f1xx_ll_usart.h>
 	
-void LL_USART_PutChar(USART_TypeDef *USARTx, char data);
+void LL_USART_TransmitChar(USART_TypeDef *USARTx, char data);
 void LL_USART_TransmitString(USART_TypeDef *USARTx, const char *str);
 
 void LL_USART_TransmitString_IT(USART_TypeDef *USARTx, const char *str);
-uint8_t LL_USART_IsSending(USART_TypeDef *USARTx);
-void LL_USART_Transmit_IRQHandler(USART_TypeDef *USARTx);
+uint8_t LL_USART_IsTransmitBusy(USART_TypeDef *USARTx);
+void LL_USART_Transmit_Handler(USART_TypeDef *USARTx);
 
 
 #ifdef __cplusplus
