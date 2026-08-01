d093b448dc1a10631963019109e64fc6c7e12d78
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-08-01 16:22:56 +0330

    chore: update, ll_usart_transmit_string

--- platforms/stm32f1/peripherals/ll_usart_transmit_string.c
+++ platforms/stm32f1/peripherals/ll_usart_transmit_string.c
@@ -5,44 +5,6 @@
 #include "stm32f1xx_ll_usart.h"
 #include "ll_usart_transmit_string.h"
 
-static const char *tx_data_p = NULL;
-static volatile uint8_t sending = 0;
-
-//********************************
-void LL_USART_TransmitString_IT(USART_TypeDef *USARTx, const char *str) {
-    if (sending == 1 || str == NULL || *str == '\0'){
-			return;
-		}
-
-    tx_data_p = str;
-    sending = 1;
-
-    LL_USART_EnableIT_TXE(USARTx);
-}
-
-//********************************
-uint8_t LL_USART_IsTransmittingStringBusy(USART_TypeDef *USARTx) {
-    return sending;
-}
-
-//********************************
-void LL_USART_TransmitString_IT_Handler(USART_TypeDef *USARTx) {
-    if (LL_USART_IsEnabledIT_TXE(USARTx) && LL_USART_IsActiveFlag_TXE(USARTx)) {
-        if (*tx_data_p != '\0') {
-            LL_USART_TransmitData8(USARTx, *tx_data_p++);
-        } else {
-            LL_USART_DisableIT_TXE(USARTx);
-            LL_USART_EnableIT_TC(USARTx);
-        }
-    }
-
-    if (LL_USART_IsEnabledIT_TC(USARTx) && LL_USART_IsActiveFlag_TC(USARTx)) {
-        LL_USART_ClearFlag_TC(USARTx);
-        LL_USART_DisableIT_TC(USARTx);
-        sending = 0; 
-    }
-}
-
 //********************************
 void LL_USART_TransmitChar(USART_TypeDef *USARTx, char data){
 	while(LL_USART_IsActiveFlag_TXE(USARTx) != 1){}		// Transmit Data Register Empty 
@@ -58,4 +20,43 @@ void LL_USART_TransmitString(USART_TypeDef *USARTx, const char *str){
   }
 //	LL_USART_TransmitChar(USARTx, '\r');		// 0x0D
 //	LL_USART_TransmitChar(USARTx, '\n');		// 0x0A
+}
+
+//********************************
+void LL_USART_TransmitString_IT(LL_USART_TransmitString_TypeDef *handle, USART_TypeDef *USARTx, const char *str) {
+    if (handle == 0 || handle->tx_busy == 1 || str == NULL || *str == '\0'){
+        return;
+    }
+
+    handle->tx_str_p = str;
+    handle->tx_busy = 1;
+
+    LL_USART_EnableIT_TXE(USARTx);
+}
+
+//********************************
+void LL_USART_TransmitString_IT_Handler(LL_USART_TransmitString_TypeDef *handle, USART_TypeDef *USARTx) {
+    if (handle == 0) {
+        return;
+    }
+
+    if (LL_USART_IsEnabledIT_TXE(USARTx) && LL_USART_IsActiveFlag_TXE(USARTx)) {
+        if (*handle->tx_str_p != '\0') {
+            LL_USART_TransmitData8(USARTx, *handle->tx_str_p++);
+        } else {
+            LL_USART_DisableIT_TXE(USARTx);
+            LL_USART_EnableIT_TC(USARTx);
+        }
+    }
+
+    if (LL_USART_IsEnabledIT_TC(USARTx) && LL_USART_IsActiveFlag_TC(USARTx)) {
+        LL_USART_ClearFlag_TC(USARTx);
+        LL_USART_DisableIT_TC(USARTx);
+        handle->tx_busy = 0;
+    }
+}
+
+//********************************
+uint8_t LL_USART_IsTransmittingStringBusy(LL_USART_TransmitString_TypeDef *handle) {
+    return handle->tx_busy;
 }
--- platforms/stm32f1/peripherals/ll_usart_transmit_string.h
+++ platforms/stm32f1/peripherals/ll_usart_transmit_string.h
@@ -7,16 +7,22 @@
 extern "C" {
 #endif
 
+
 #include <stdint.h>
 #include <stm32f1xx.h>
 #include <stm32f1xx_ll_usart.h>
-	
+
+typedef struct{
+    const char *tx_str_p;
+    volatile uint8_t tx_busy;
+} LL_USART_TransmitString_TypeDef;
+
 void LL_USART_TransmitChar(USART_TypeDef *USARTx, char data);
 void LL_USART_TransmitString(USART_TypeDef *USARTx, const char *str);
 
-void LL_USART_TransmitString_IT(USART_TypeDef *USARTx, const char *str);
-void LL_USART_TransmitString_IT_Handler(USART_TypeDef *USARTx);
-uint8_t LL_USART_IsTransmittingStringBusy(USART_TypeDef *USARTx);
+void LL_USART_TransmitString_IT(LL_USART_TransmitString_TypeDef *handle, USART_TypeDef *USARTx, const char *str);
+void LL_USART_TransmitString_IT_Handler(LL_USART_TransmitString_TypeDef *handle, USART_TypeDef *USARTx);
+uint8_t LL_USART_IsTransmittingStringBusy(LL_USART_TransmitString_TypeDef *handle);
 
 
 #ifdef __cplusplus
