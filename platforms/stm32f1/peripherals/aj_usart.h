36beca2b6378fd367fa58544b1bc4fa8e075cbb2
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-08-06 00:04:58 +0330

    Update aj_usart.h

--- platforms/stm32f1/peripherals/aj_usart.h
+++ platforms/stm32f1/peripherals/aj_usart.h
@@ -1,3 +1,18 @@
+/**
+ * @brief   Universal synchronous/asynchronous receiver transmitter (USART) interface for STM32 peripherals.
+ *
+ * This file provides helper APIs for managing USART control, status and data registers, including:
+ * - USARTx->SR
+ * - USARTx->DR
+ * - USARTx->BRR
+ * - USARTx->CR1
+ * - USARTx->CR2
+ * - USARTx->CR3
+ *
+ * @author  AliReza Joodi
+ * @see     https://github.com/AliRezaJoodi
+ */
+
 #ifndef AJ_USART_INCLUDED
 #define AJ_USART_INCLUDED
 
@@ -24,11 +39,19 @@ void AJ_USART_ConfigBaudRate_8x(USART_TypeDef *USARTx, uint32_t periphclk, uint3
 
 void AJ_USART_ConfigBaudRate_16x(USART_TypeDef *USARTx, uint32_t periphclk, uint32_t baudrate);
 
+/******************************************************************************/
+/* Status register (USART_SR)                                                 */
+/******************************************************************************/
 static inline uint8_t AJ_USART_IsFlagActive_ParityError(const USART_TypeDef *USARTx){
   return AJ_BitReg_IsBitSet_Mask(&(USARTx->SR), USART_SR_PE_Msk);
 }
 
 static inline void AJ_USART_ClearFlag_ParityError(USART_TypeDef *USARTx){
+  volatile uint32_t tmpreg;
+  tmpreg = USARTx->SR;
+  (void) tmpreg;
+  tmpreg = USARTx->DR;
+  (void) tmpreg;
 }
 
 static inline uint8_t AJ_USART_IsFlagActive_FramingError(const USART_TypeDef *USARTx){
@@ -36,6 +59,11 @@ static inline uint8_t AJ_USART_IsFlagActive_FramingError(const USART_TypeDef *US
 }
 
 static inline void AJ_USART_ClearFlag_FramingError(USART_TypeDef *USARTx){
+  volatile uint32_t tmpreg;
+  tmpreg = USARTx->SR;
+  (void) tmpreg;
+  tmpreg = USARTx->DR;
+  (void) tmpreg;
 }
 
 static inline uint8_t AJ_USART_IsFlagActive_NoiseError(const USART_TypeDef *USARTx){
@@ -43,6 +71,11 @@ static inline uint8_t AJ_USART_IsFlagActive_NoiseError(const USART_TypeDef *USAR
 }
 
 static inline void AJ_USART_ClearFlag_NoiseError(USART_TypeDef *USARTx){
+  volatile uint32_t tmpreg;
+  tmpreg = USARTx->SR;
+  (void) tmpreg;
+  tmpreg = USARTx->DR;
+  (void) tmpreg;
 }
 
 static inline uint8_t AJ_USART_IsFlagActive_OverrunError(const USART_TypeDef *USARTx){
@@ -50,6 +83,11 @@ static inline uint8_t AJ_USART_IsFlagActive_OverrunError(const USART_TypeDef *US
 }
 
 static inline void AJ_USART_ClearFlag_OverrunError(USART_TypeDef *USARTx){
+  volatile uint32_t tmpreg;
+  tmpreg = USARTx->SR;
+  (void) tmpreg;
+  tmpreg = USARTx->DR;
+  (void) tmpreg;
 }
 
 static inline uint8_t AJ_USART_IsFlagActive_IdleLine(const USART_TypeDef *USARTx){
@@ -57,6 +95,11 @@ static inline uint8_t AJ_USART_IsFlagActive_IdleLine(const USART_TypeDef *USARTx
 }
 
 static inline void AJ_USART_ClearFlag_IdleLine(USART_TypeDef *USARTx){
+  volatile uint32_t tmpreg;
+  tmpreg = USARTx->SR;
+  (void) tmpreg;
+  tmpreg = USARTx->DR;
+  (void) tmpreg;
 }
 
 static inline uint8_t AJ_USART_IsFlagActive_ReceiveDataNotEmpty(const USART_TypeDef *USARTx){
@@ -64,6 +107,7 @@ static inline uint8_t AJ_USART_IsFlagActive_ReceiveDataNotEmpty(const USART_Type
 }
 
 static inline void AJ_USART_ClearFlag_ReceiveDataNotEmpty(USART_TypeDef *USARTx){
+  USARTx->SR = ~(USART_SR_RXNE_Msk);
 }
 
 static inline uint8_t AJ_USART_IsFlagActive_TransmissionComplete(const USART_TypeDef *USARTx){
@@ -71,6 +115,7 @@ static inline uint8_t AJ_USART_IsFlagActive_TransmissionComplete(const USART_Typ
 }
 
 static inline void AJ_USART_ClearFlag_TransmissionComplete(USART_TypeDef *USARTx){
+  USARTx->SR = ~(USART_SR_TC_Msk);
 }
 
 static inline uint8_t AJ_USART_IsFlagActive_TransmitDataEmpty(const USART_TypeDef *USARTx){
@@ -82,6 +127,7 @@ static inline uint8_t AJ_USART_IsFlagActive_LinBreakDetection(const USART_TypeDe
 }
 
 static inline void AJ_USART_ClearFlag_LinBreakDetection(USART_TypeDef *USARTx){
+  USARTx->SR = ~(USART_SR_LBD_Msk);
 }
 
 static inline uint8_t AJ_USART_IsFlagActive_CTS(const USART_TypeDef *USARTx){
@@ -89,6 +135,7 @@ static inline uint8_t AJ_USART_IsFlagActive_CTS(const USART_TypeDef *USARTx){
 }
 
 static inline void AJ_USART_ClearFlag_CTS(USART_TypeDef *USARTx){
+  USARTx->SR = ~(USART_SR_CTS_Msk);
 }
 
 
