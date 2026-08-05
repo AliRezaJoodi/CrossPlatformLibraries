c7718ae622824bbf5dd747279aa3b1f801bbcb12
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-08-05 23:34:55 +0330

    Update aj_usart.h

--- platforms/stm32f1/peripherals/aj_usart.h
+++ platforms/stm32f1/peripherals/aj_usart.h
@@ -25,67 +25,67 @@ void AJ_USART_ConfigBaudRate_8x(USART_TypeDef *USARTx, uint32_t periphclk, uint3
 void AJ_USART_ConfigBaudRate_16x(USART_TypeDef *USARTx, uint32_t periphclk, uint32_t baudrate);
 
 static inline uint8_t AJ_USART_IsFlagActive_ParityError(const USART_TypeDef *USARTx){
-  return 0;
+  return AJ_BitReg_IsBitSet_Mask(&(USARTx->SR), USART_SR_PE_Msk);
 }
 
 static inline void AJ_USART_ClearFlag_ParityError(USART_TypeDef *USARTx){
 }
 
 static inline uint8_t AJ_USART_IsFlagActive_FramingError(const USART_TypeDef *USARTx){
-  return 0;
+  return AJ_BitReg_IsBitSet_Mask(&(USARTx->SR), USART_SR_FE_Msk);
 }
 
 static inline void AJ_USART_ClearFlag_FramingError(USART_TypeDef *USARTx){
 }
 
 static inline uint8_t AJ_USART_IsFlagActive_NoiseError(const USART_TypeDef *USARTx){
-  return 0;
+  return AJ_BitReg_IsBitSet_Mask(&(USARTx->SR), USART_SR_NE_Msk);
 }
 
 static inline void AJ_USART_ClearFlag_NoiseError(USART_TypeDef *USARTx){
 }
 
 static inline uint8_t AJ_USART_IsFlagActive_OverrunError(const USART_TypeDef *USARTx){
-  return 0;
+  return AJ_BitReg_IsBitSet_Mask(&(USARTx->SR), USART_SR_ORE_Msk);
 }
 
 static inline void AJ_USART_ClearFlag_OverrunError(USART_TypeDef *USARTx){
 }
 
 static inline uint8_t AJ_USART_IsFlagActive_IdleLine(const USART_TypeDef *USARTx){
-  return 0;
+  return AJ_BitReg_IsBitSet_Mask(&(USARTx->SR), USART_SR_IDLE_Msk);
 }
 
 static inline void AJ_USART_ClearFlag_IdleLine(USART_TypeDef *USARTx){
 }
 
 static inline uint8_t AJ_USART_IsFlagActive_ReceiveDataNotEmpty(const USART_TypeDef *USARTx){
-  return 0;
+  return AJ_BitReg_IsBitSet_Mask(&(USARTx->SR), USART_SR_RXNE_Msk);
 }
 
 static inline void AJ_USART_ClearFlag_ReceiveDataNotEmpty(USART_TypeDef *USARTx){
 }
 
 static inline uint8_t AJ_USART_IsFlagActive_TransmissionComplete(const USART_TypeDef *USARTx){
-  return 0;
+  return AJ_BitReg_IsBitSet_Mask(&(USARTx->SR), USART_SR_TC_Msk);
 }
 
 static inline void AJ_USART_ClearFlag_TransmissionComplete(USART_TypeDef *USARTx){
 }
 
 static inline uint8_t AJ_USART_IsFlagActive_TransmitDataEmpty(const USART_TypeDef *USARTx){
-  return 0;
+  return AJ_BitReg_IsBitSet_Mask(&(USARTx->SR), USART_SR_TXE_Msk);
 }
 
 static inline uint8_t AJ_USART_IsFlagActive_LinBreakDetection(const USART_TypeDef *USARTx){
-  return 0;
+  return AJ_BitReg_IsBitSet_Mask(&(USARTx->SR), USART_SR_LBD_Msk);
 }
 
 static inline void AJ_USART_ClearFlag_LinBreakDetection(USART_TypeDef *USARTx){
 }
 
 static inline uint8_t AJ_USART_IsFlagActive_CTS(const USART_TypeDef *USARTx){
-  return 0;
+  return AJ_BitReg_IsBitSet_Mask(&(USARTx->SR), USART_SR_CTS_Msk);
 }
 
 static inline void AJ_USART_ClearFlag_CTS(USART_TypeDef *USARTx){
