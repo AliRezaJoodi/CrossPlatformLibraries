e650158db741634a4cff8d43d4f254f9e6d3b36b
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-08-06 00:11:25 +0330

    Update aj_usart.h

--- platforms/stm32f1/peripherals/aj_usart.h
+++ platforms/stm32f1/peripherals/aj_usart.h
@@ -33,6 +33,9 @@ void AJ_USART_ConfigOverSampling(USART_TypeDef *USARTx, aj_usart_oversampling_t 
 aj_usart_oversampling_t AJ_USART_ReadOverSampling(const USART_TypeDef *USARTx);
 #endif
 
+/******************************************************************************/
+/* Baud rate register (USART_BRR)                                             */
+/******************************************************************************/
 #if defined(USART_CR1_OVER8_Msk)
 void AJ_USART_ConfigBaudRate_8x(USART_TypeDef *USARTx, uint32_t periphclk, uint32_t baudrate);
 #endif
