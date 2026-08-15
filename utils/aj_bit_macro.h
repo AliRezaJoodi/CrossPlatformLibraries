d2f6ce0f4871a387b21e3ce76d626cdc32bc5d1d
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-08-15 17:00:03 +0330

    Update aj_bit_macro.h

--- utils/aj_bit_macro.h
+++ utils/aj_bit_macro.h
@@ -21,9 +21,9 @@
  * @subsection pos_reqs Position-based Operations
  * - The `pos` parameter is 0-indexed.
  * - The valid range of `pos` depends on the width of the target object:
- *   - 8-bit  object: 0–7
- *   - 16-bit object: 0–15
- *   - 32-bit object: 0–31
+ *   - 8-bit  object: 0ï¿½7
+ *   - 16-bit object: 0ï¿½15
+ *   - 32-bit object: 0ï¿½31
  *
  * @subsection macro_notes Macro Behavior
  * - These utilities are implemented as macros and may evaluate arguments
@@ -42,14 +42,15 @@
 extern "C" {
 #endif
 
-#define AJ_SET_BITS_MASK(ADDRESS, MASK)             ((ADDRESS) |= (MASK))
-#define AJ_SET_BIT_POS(ADDRESS, POS)                ((ADDRESS) |= (0x01UL << (POS)))
 
-#define AJ_CLEAR_BITS_MASK(ADDRESS, MASK)           ((ADDRESS) &= ~(MASK))
-#define AJ_CLEAR_BIT_POS(ADDRESS, POS)              ((ADDRESS) &= ~(0x01UL << (POS)))
+#define AJ_SET_BIT_MASK(ADDRESS, MASK)             ((ADDRESS) |= (MASK))
+#define AJ_SET_BIT_POS(ADDRESS, POS)               ((ADDRESS) |= (0x01UL << (POS)))
 
-#define AJ_TOGGLE_BITS_MASK(ADDRESS, MASK)          ((ADDRESS) ^= (MASK))
-#define AJ_TOGGLE_BIT_POS(ADDRESS, POS)             ((ADDRESS) ^= (0x01UL << (POS)))
+#define AJ_CLEAR_BIT_MASK(ADDRESS, MASK)           ((ADDRESS) &= ~(MASK))
+#define AJ_CLEAR_BIT_POS(ADDRESS, POS)             ((ADDRESS) &= ~(0x01UL << (POS)))
+
+#define AJ_TOGGLE_BIT_MASK(ADDRESS, MASK)          ((ADDRESS) ^= (MASK))
+#define AJ_TOGGLE_BIT_POS(ADDRESS, POS)            ((ADDRESS) ^= (0x01UL << (POS)))
 
 #if defined(__GNUC__) || defined(__clang__)
 /* MASK must not be 0 */
@@ -57,28 +58,29 @@ extern "C" {
     ((ADDRESS) = (((ADDRESS) & ~(MASK)) | (((VALUE) << (__builtin_ctz(MASK))) & (MASK))))
 #endif
 
-#define AJ_WRITE_BIT_POS(ADDRESS, POS, VALUE)       ((ADDRESS) = ((ADDRESS) & ~(0x01UL << (POS))) | ((0x01UL & (VALUE)) << (POS)))
-#define AJ_WRITE_2BITS_POS(ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0x03UL << (POS))) | ((0x03UL & (VALUE)) << (POS)))
-#define AJ_WRITE_3BITS_POS(ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0x07UL << (POS))) | ((0x07UL & (VALUE)) << (POS)))
-#define AJ_WRITE_4BITS_POS(ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0x0FUL << (POS))) | ((0x0FUL & (VALUE)) << (POS)))
-#define AJ_WRITE_5BITS_POS(ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0x1FUL << (POS))) | ((0x1FUL & (VALUE)) << (POS)))
-#define AJ_WRITE_6BITS_POS(ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0x3FUL << (POS))) | ((0x3FUL & (VALUE)) << (POS)))
-#define AJ_WRITE_7BITS_POS(ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0x7FUL << (POS))) | ((0x7FUL & (VALUE)) << (POS)))
-#define AJ_WRITE_8BITS_POS(ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0xFFUL << (POS))) | ((0xFFUL & (VALUE)) << (POS)))
+#define AJ_WRITE_BIT_POS(ADDRESS, POS, VALUE)      ((ADDRESS) = ((ADDRESS) & ~(0x01UL << (POS))) | ((0x01UL & (VALUE)) << (POS)))
+#define AJ_WRITE_2BIT_POS(ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0x03UL << (POS))) | ((0x03UL & (VALUE)) << (POS)))
+#define AJ_WRITE_3BIT_POS(ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0x07UL << (POS))) | ((0x07UL & (VALUE)) << (POS)))
+#define AJ_WRITE_4BIT_POS(ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0x0FUL << (POS))) | ((0x0FUL & (VALUE)) << (POS)))
+#define AJ_WRITE_5BIT_POS(ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0x1FUL << (POS))) | ((0x1FUL & (VALUE)) << (POS)))
+#define AJ_WRITE_6BIT_POS(ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0x3FUL << (POS))) | ((0x3FUL & (VALUE)) << (POS)))
+#define AJ_WRITE_7BIT_POS(ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0x7FUL << (POS))) | ((0x7FUL & (VALUE)) << (POS)))
+#define AJ_WRITE_8BIT_POS(ADDRESS, POS, VALUE)     ((ADDRESS) = ((ADDRESS) & ~(0xFFUL << (POS))) | ((0xFFUL & (VALUE)) << (POS)))
 
 #if defined(__GNUC__) || defined(__clang__)
 /* mask must not be 0 */
 #define AJ_GET_FIELD_MASK(ADDRESS, MASK)(        	((ADDRESS) & (MASK)) >> (__builtin_ctz(MASK)))
 #endif
 
-#define AJ_GET_BIT_POS(ADDRESS, POS)                (((ADDRESS) >> (POS)) & 0x01UL)
-#define AJ_GET_2BITS_POS(ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0x03UL)
-#define AJ_GET_3BITS_POS(ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0x07UL)
-#define AJ_GET_4BITS_POS(ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0x0FUL)
-#define AJ_GET_5BITS_POS(ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0x1FUL)
-#define AJ_GET_6BITS_POS(ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0x3FUL)
-#define AJ_GET_7BITS_POS(ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0x7FUL)
-#define AJ_GET_8BITS_POS(ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0xFFUL)
+#define AJ_GET_BIT_POS(ADDRESS, POS)               (((ADDRESS) >> (POS)) & 0x01UL)
+#define AJ_GET_2BIT_POS(ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0x03UL)
+#define AJ_GET_3BIT_POS(ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0x07UL)
+#define AJ_GET_4BIT_POS(ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0x0FUL)
+#define AJ_GET_5BIT_POS(ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0x1FUL)
+#define AJ_GET_6BIT_POS(ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0x3FUL)
+#define AJ_GET_7BIT_POS(ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0x7FUL)
+#define AJ_GET_8BIT_POS(ADDRESS, POS)              (((ADDRESS) >> (POS)) & 0xFFUL)
+
 
 #ifdef __cplusplus
 }
