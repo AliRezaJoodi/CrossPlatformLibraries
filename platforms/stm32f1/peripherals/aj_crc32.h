08de985105f4c38bef20107814b1c1624a146266
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-08-17 11:45:46 +0330

    chore: initial setup, aj_crc32

---
+++ platforms/stm32f1/peripherals/aj_crc32.h
@@ -0,0 +1,26 @@
+/**
+ * @brief   CRC-32 calculation interface for STM32F1.
+ *
+ * This file provides helper APIs for using the CRC peripheral of STM32F1.
+ *
+ * @author  AliReza Joodi
+ * @see     https://github.com/AliRezaJoodi
+ */
+
+#ifndef AJ_CRC32_INCLUDED
+#define AJ_CRC32_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#include <stdint.h>
+#include <stm32f1xx.h>
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* AJ_CRC32_INCLUDED */
