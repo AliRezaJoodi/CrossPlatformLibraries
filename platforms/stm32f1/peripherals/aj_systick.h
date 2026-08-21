6d5d1317ad07d0c403f5d163b0d8ded0d6177be0
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-08-21 22:21:52 +0330

    chore: initial setup, aj_systick

---
+++ platforms/stm32f1/peripherals/aj_systick.h
@@ -0,0 +1,25 @@
+/**
+ * @brief SysTick utility library for STM32F1xx microcontrollers.
+ *
+ * @author  AliReza Joodi
+ * @see     https://github.com/AliRezaJoodi
+ */
+
+#ifndef AJ_SYSTICK_INCLUDED
+#define AJ_SYSTICK_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#include <stdint.h>
+#include <stm32f1xx.h>
+#include "aj_systick_type.h"
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	/* AJ_SYSTICK_INCLUDED */
---
+++ platforms/stm32f1/peripherals/aj_systick_type.h
@@ -0,0 +1,16 @@
+#ifndef AJ_SYSTICK_TYPE_INCLUDED
+#define AJ_SYSTICK_TYPE_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#include <stdint.h>
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	/* AJ_SYSTICK_TYPE_INCLUDED */
