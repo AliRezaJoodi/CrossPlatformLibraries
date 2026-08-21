7e37b86e12103972307528d1a087b4d9084c4db6
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-08-22 00:04:01 +0330

    Update aj_systick.h

--- platforms/stm32f1/peripherals/aj_systick.h
+++ platforms/stm32f1/peripherals/aj_systick.h
@@ -15,7 +15,20 @@ extern "C" {
 
 #include <stdint.h>
 #include <stm32f1xx.h>
+#include "aj_bit_reg.h"
 #include "aj_systick_type.h"
+
+/******************************************************************************/
+/* Count Flag                                                                 */
+/******************************************************************************/
+static inline uint8_t AJ_SysTick_IsFlagActive(void){
+	return AJ_BitReg_IsBitSet_Mask(&(SysTick->CTRL), SysTick_CTRL_COUNTFLAG_Msk);
+}
+
+static inline void AJ_SysTick_ClearFlag(void){
+	volatile uint32_t tmp = SysTick->CTRL;
+	((void)tmp);
+}
 
 
 #ifdef __cplusplus
