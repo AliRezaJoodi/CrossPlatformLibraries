2404e46359fb524c6f3afc0f4ceb7e375c26028a
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-09-03 16:00:35 +0330

    chore: update, aj_target

--- platforms/avr8/ports/aj_target.h
+++ platforms/avr8/ports/aj_target.h
@@ -28,10 +28,11 @@ extern "C" {
 
 #include "hardware.h"
 
+#define AJ_TARGET_MCU_BITS              8U
+
 #ifndef AJ_TARGET_CONFIG
 #define AJ_TARGET_CONFIG
-    #define AJ_TARGET_MCU_BITS              32U     /*!< Supported: 8, 16, 32 */
-    #define AJ_TARGET_TICK_BITS             32U     /*!< Supported: 8, 16, 32 */
+    #define AJ_TARGET_TICK_BITS             16U     /*!< Supported: 8, 16, 32 */
 
     #warning "AJ_TARGET_CONFIG is not defined; default configuration will be used."
 #endif
@@ -39,13 +40,6 @@ extern "C" {
 /******************************************************************************/
 /* Compile-time configuration validation                                      */
 /******************************************************************************/
-#if (AJ_TARGET_MCU_BITS != 8U)  && \
-    (AJ_TARGET_MCU_BITS != 16U) && \
-    (AJ_TARGET_MCU_BITS != 32U)
-
-    #error "AJ_TARGET_MCU_BITS must be 8U, 16U, or 32U."
-#endif
-
 #if (AJ_TARGET_TICK_BITS != 8U)  && \
     (AJ_TARGET_TICK_BITS != 16U) && \
     (AJ_TARGET_TICK_BITS != 32U)
