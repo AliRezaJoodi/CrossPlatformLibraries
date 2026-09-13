33e6b580db86a07c382b68e4a1b70b7cfc9be105
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-09-13 18:56:37 +0330

    Update aj_afio.h

--- platforms/stm32f1/peripherals/aj_afio.h
+++ platforms/stm32f1/peripherals/aj_afio.h
@@ -1,3 +1,45 @@
+/**
+ * @brief   Alternate function I/O (AFIO) interface for STM32 peripherals.
+ *
+ * This file provides helper APIs for managing AFIO control registers,
+ * including:
+ * - AFIO->MAPR
+ *
+ * -----------------------------------------------------------------------------
+ * REQUIREMENT 1: Application Setup (BUS Configuration)
+ * -----------------------------------------------------------------------------
+ * This library does not enable any peripheral clock.
+ * The application must enable the required clock buses before using this driver.
+ * The following clock bus must be enabled:
+ * - The APB2 clock of the AFIO peripheral
+ *
+ * -----------------------------------------------------------------------------
+ * REQUIREMENT 2: Default Configuration
+ * -----------------------------------------------------------------------------
+ * The default driver macros are declared in the following headers:
+ * - `aj_target.h`
+ *
+ * -----------------------------------------------------------------------------
+ * REQUIREMENT 3: User Configuration Override
+ * -----------------------------------------------------------------------------
+ * This library's default configuration can be customized via the central
+ * project hardware configuration file, which MUST be present alongside your
+ * project source files. Override the macros inside it:
+ * - `hardware.h`
+ *
+ * -----------------------------------------------------------------------------
+ * EXAMPLE PROJECT
+ * -----------------------------------------------------------------------------
+ * @see    STM32F103 training exercise link:
+ *         https://github.com/AliRezaJoodi/STM32_Exercises/tree/main/AFIO_F103/BareMetal
+ *     
+ * -----------------------------------------------------------------------------
+ * Source
+ * -----------------------------------------------------------------------------
+ * @author  AliRezaJoodi
+ * @see     https://github.com/AliRezaJoodi
+ */
+
 #ifndef AJ_AFIO_INCLUDED
 #define AJ_AFIO_INCLUDED
 
